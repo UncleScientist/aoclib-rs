c4b5be60ef908392451d6d4697b161b58a299bec
Uncle Scientist <unclescientist@gmail.com>
2022-08-22 08:40:45 -0400

    solved part 2 and made a template for remaining parts

--- src/bin/run-aoc/aoc2015/mod.rs
+++ src/bin/run-aoc/aoc2015/mod.rs
@@ -1,8 +1,18 @@
 mod aoc2015_01;
+mod aoc2015_02;
+mod aoc2015_03;
 
 pub use aoc2015_01::*;
+pub use aoc2015_02::*;
+pub use aoc2015_03::*;
 
 pub fn run_2015() {
-    let mut x = Aoc2015_01::new();
-    crate::run_solution(&mut x);
+    let mut day01 = Aoc2015_01::new();
+    crate::run_solution(&mut day01);
+
+    let mut day02 = Aoc2015_02::new();
+    crate::run_solution(&mut day02);
+
+    let mut day03 = Aoc2015_03::new();
+    crate::run_solution(&mut day03);
 }
