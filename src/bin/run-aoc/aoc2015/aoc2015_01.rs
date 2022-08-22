ec4f25816d1f284f583540ad0a5c4670124555a2
Uncle Scientist <unclescientist@gmail.com>
2022-08-22 08:09:32 -0400

    A slightly better layout

--- src/bin/run-aoc/aoc2015/aoc2015_01.rs
+++ src/bin/run-aoc/aoc2015/aoc2015_01.rs
@@ -1,15 +1,50 @@
 use aoclib_rs::read_to_chars;
 
-pub fn aoc2015_01() {
-    let data = read_to_chars("input/2015-01.txt");
-    println!(
-        "2015 day 1 part 1 = {}",
-        data.iter()
-            .map(|x| match x {
+pub struct Aoc2015_01 {
+    data: Vec<char>,
+}
+
+impl Aoc2015_01 {
+    pub fn new() -> Self {
+        Self {
+            data: read_to_chars("input/2015-01.txt"),
+        }
+    }
+}
+
+impl crate::Runner for Aoc2015_01 {
+    fn name(&self) -> String {
+        "2015 Day 1".to_string()
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        vec![format!(
+            "{}",
+            self.data
+                .iter()
+                .map(|x| match x {
+                    '(' => 1,
+                    ')' => -1,
+                    _ => panic!("invalid char in input"),
+                })
+                .sum::<i32>()
+        )]
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        let mut cur_floor = 0;
+        for (pos, c) in self.data.iter().enumerate() {
+            let step = match c {
                 '(' => 1,
                 ')' => -1,
-                _ => panic!("invalid char in input"),
-            })
-            .sum::<i32>()
-    );
+                _ => panic!("invalid character input"),
+            };
+            cur_floor += step;
+            if cur_floor < 0 {
+                return vec![format!("{}", pos + 1)];
+            }
+        }
+
+        panic!("no answer found");
+    }
 }
--- src/bin/run-aoc/main.rs
+++ src/bin/run-aoc/main.rs
@@ -1,6 +1,32 @@
 mod aoc2015;
 use aoc2015::*;
 
+pub trait Runner {
+    fn part1(&mut self) -> Vec<String>;
+    fn part2(&mut self) -> Vec<String>;
+    fn name(&self) -> String;
+}
+
 fn main() {
-    aoc2015_01();
+    run_2015();
+}
+
+pub fn run_solution<T: Runner>(solution: &mut T) {
+    println!("---- {} ----", solution.name());
+
+    let v1 = solution.part1();
+    let mut p1 = v1.iter();
+    let first = p1.next().unwrap();
+    println!("Part 1: {first}");
+    while let Some(line) = p1.next() {
+        println!("       {line}");
+    }
+
+    let v2 = solution.part2();
+    let mut p2 = v2.iter();
+    let first = p2.next().unwrap();
+    println!("Part 2: {first}");
+    while let Some(line) = p2.next() {
+        println!("       {line}");
+    }
 }
