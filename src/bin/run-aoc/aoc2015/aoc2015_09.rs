7a5a8bf5d14d056ffb3c720d2df6515c7fba1c4a
Uncle Scientist <unclescientist@gmail.com>
2022-08-29 08:00:33 -0400

    generic permutation generator

--- src/bin/run-aoc/aoc2015/aoc2015_09.rs
+++ src/bin/run-aoc/aoc2015/aoc2015_09.rs
@@ -1,4 +1,4 @@
-use aoclib::read_lines;
+use aoclib::{read_lines, PermutationIterator};
 use std::collections::{HashMap, HashSet};
 
 use crate::Runner;
@@ -34,45 +34,23 @@ impl Aoc2015_09 {
     }
 }
 
-struct Perms<T> {
-    list: Vec<Vec<T>>,
-}
-
-impl<T: Clone> Perms<T> {
-    fn generate(&mut self, vec: &mut [T], size: usize) {
-        if size == 1 {
-            self.list.push(Vec::from(vec));
-        } else {
-            self.generate(vec, size - 1);
-
-            for i in 0..size - 1 {
-                if size % 2 == 0 {
-                    vec.swap(i, size - 1)
-                } else {
-                    vec.swap(0, size - 1);
-                }
-                self.generate(vec, size - 1);
-            }
-        }
-    }
-}
-
 impl Runner for Aoc2015_09 {
     fn name(&self) -> (usize, usize) {
         (2015, 9)
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut permutations = Perms { list: Vec::new() };
-        let mut city_list = self
-            .cities
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>();
-        permutations.generate(&mut city_list, self.cities.len());
+        let iter = PermutationIterator::new(
+            &self
+                .cities
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+        );
 
         let mut shortest = u64::MAX;
-        for p in permutations.list.iter() {
+        for p in iter {
+            // permutations.list.iter() {
             let mut total = 0;
             for pair in p.windows(2) {
                 total += self.dist.get(&(pair[0].clone(), pair[1].clone())).unwrap();
