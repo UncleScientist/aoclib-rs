590dd861ed63bacaf356b67f5b68d41d83437c29
Uncle Scientist <unclescientist@gmail.com>
2022-08-29 08:20:01 -0400

    2015 day 10

--- src/bin/run-aoc/aoc2015/mod.rs
+++ src/bin/run-aoc/aoc2015/mod.rs
@@ -9,6 +9,7 @@ mod aoc2015_06;
 mod aoc2015_07;
 mod aoc2015_08;
 mod aoc2015_09;
+mod aoc2015_10;
 
 pub use aoc2015_01::*;
 pub use aoc2015_02::*;
@@ -19,6 +20,7 @@ pub use aoc2015_06::*;
 pub use aoc2015_07::*;
 pub use aoc2015_08::*;
 pub use aoc2015_09::*;
+pub use aoc2015_10::*;
 
 pub fn run_2015(which: Selector) {
     let mut day01 = Aoc2015_01::new();
@@ -30,10 +32,11 @@ pub fn run_2015(which: Selector) {
     let mut day07 = Aoc2015_07::new();
     let mut day08 = Aoc2015_08::new();
     let mut day09 = Aoc2015_09::new();
+    let mut day10 = Aoc2015_10::new();
 
     let mut days: Vec<&mut dyn Runner> = vec![
         &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
-        &mut day08, &mut day09,
+        &mut day08, &mut day09, &mut day10,
     ];
 
     match which {
