fc2958365c231381d7769502feafe0fffeaab5fb
Uncle Scientist <unclescientist@gmail.com>
2022-08-29 08:20:01 -0400

    2015 day 10

---
+++ src/bin/run-aoc/aoc2015/aoc2015_10.rs
@@ -0,0 +1,75 @@
+use crate::Runner;
+
+pub struct Aoc2015_10 {
+    part_1_result: String,
+}
+
+impl Aoc2015_10 {
+    pub fn new() -> Self {
+        Self {
+            part_1_result: String::from(""),
+        }
+    }
+}
+
+impl Runner for Aoc2015_10 {
+    fn name(&self) -> (usize, usize) {
+        (2015, 10)
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        self.part_1_result = String::from("1321131112");
+        for _ in 0..40 {
+            self.part_1_result = one_round(&self.part_1_result);
+        }
+        crate::output(self.part_1_result.len())
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        let mut part2 = self.part_1_result.clone();
+        for _ in 0..10 {
+            part2 = one_round(&part2);
+        }
+        crate::output(part2.len())
+    }
+}
+
+fn one_round(input: &String) -> String {
+    let mut result = String::from("");
+    let mut iter = input.chars().peekable();
+
+    while let Some(cur) = iter.next() {
+        let mut count = 1;
+        while let Some(next) = iter.peek() {
+            if *next == cur {
+                count += 1;
+                iter.next();
+            } else {
+                break;
+            }
+        }
+        result.push_str(format!("{count}{cur}").as_str());
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_rounds() {
+        let mut say = String::from("1");
+        say = one_round(&say);
+        assert_eq!(say, "11".to_string());
+        say = one_round(&say);
+        assert_eq!(say, "21".to_string());
+        say = one_round(&say);
+        assert_eq!(say, "1211".to_string());
+        say = one_round(&say);
+        assert_eq!(say, "111221".to_string());
+        say = one_round(&say);
+        assert_eq!(say, "312211".to_string());
+    }
+}
