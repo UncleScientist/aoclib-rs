7f651309b41b4b93facba3686b7b3efba98d42cb
Uncle Scientist <unclescientist@gmail.com>
2022-09-06 08:49:35 -0400

    2015 day 17 part 2

--- src/bin/run-aoc/aoc2015/aoc2015_17.rs
+++ src/bin/run-aoc/aoc2015/aoc2015_17.rs
@@ -1,4 +1,5 @@
 use crate::Runner;
+use aoclib::Combinations;
 
 pub struct Aoc2015_17 {
     cap: Vec<i32>,
@@ -23,64 +24,22 @@ impl Runner for Aoc2015_17 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output(combinations(&self.cap))
+        let mut count = 0;
+        for c in self.cap.combinations() {
+            if c.iter().sum::<i32>() == 150 {
+                count += 1;
+            }
+        }
+        crate::output(count)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
-    }
-}
-
-fn sum_eggnog(vec: &[i32], idx: &[usize]) -> i32 {
-    idx.iter().fold(0, |a, b| a + vec[*b])
-}
-
-// starting with [A, B, C, D]
-//  A   A, B    A, B, C     A, B, C, D
-//  B   A, C    A, B, D
-//  C   A, D    A, C, D
-//  D   B, C    B, C, D
-//      B, D
-//      C, D
-fn combinations(vec: &[i32]) -> usize {
-    let mut count = 0;
-    for combo in 1..=vec.len() {
-        let mut idx = Vec::with_capacity(combo);
-        for i in 0..combo {
-            idx.push(i);
-        }
-
-        if sum_eggnog(vec, &idx) == 150 {
-            count += 1;
-        }
-
-        loop {
-            let mut done = true;
-            'inc: for inc_index in (0..combo).rev() {
-                idx[inc_index] += 1;
-                if idx[inc_index] == vec.len() {
-                    continue;
-                }
-
-                for next in inc_index + 1..combo {
-                    idx[next] = idx[next - 1] + 1;
-                    if idx[next] == vec.len() {
-                        continue 'inc;
-                    }
-                }
-                done = false;
-                break; // for inc_index...
-            }
-
-            if !done {
-                if sum_eggnog(vec, &idx) == 150 {
-                    count += 1;
-                }
-            } else {
-                break;
+        let mut count = vec![0; self.cap.len()];
+        for c in self.cap.combinations() {
+            if c.iter().sum::<i32>() == 150 {
+                count[c.len()] += 1;
             }
         }
+        crate::output(count.iter().filter(|&x| *x != 0).min().unwrap())
     }
-
-    count
 }
