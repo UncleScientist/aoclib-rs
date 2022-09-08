9f109cd2152509b34d5a73ef6191af5e131d3992
Uncle Scientist <unclescientist@gmail.com>
2022-09-08 08:42:24 -0400

    improved combination algorithm

--- src/combo.rs
+++ src/combo.rs
@@ -10,23 +10,15 @@ impl<T: Clone> Combinations<T> for Vec<T> {
 
 pub struct CombinationIterator<T> {
     vec: Vec<T>,
-    combo: usize,
-    idx: Vec<usize>,
-    first: bool,
+    current: u128,
 }
 
 impl<T: Clone> CombinationIterator<T> {
     fn new(vec: &[T]) -> Self {
         Self {
             vec: Vec::from(vec),
-            combo: 1,
-            idx: Vec::new(),
-            first: true,
+            current: (1 << vec.len()) - 1,
         }
-    }
-
-    fn combos(&self) -> Vec<T> {
-        self.idx.iter().map(|&idx| self.vec[idx].clone()).collect()
     }
 }
 
@@ -34,47 +26,64 @@ impl<T: Clone> Iterator for CombinationIterator<T> {
     type Item = Vec<T>;
 
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        loop {
-            if self.first {
-                self.idx = Vec::new();
-                for idx in 0..self.combo {
-                    self.idx.push(idx);
-                }
-                self.first = false;
-                return Some(self.combos());
-            }
-
-            let mut done = false;
-            while !done {
-                done = true;
-                'inc: for inc_index in (0..self.combo).rev() {
-                    self.idx[inc_index] += 1;
-                    if self.idx[inc_index] == self.vec.len() {
-                        continue;
-                    }
-
-                    for next in inc_index + 1..self.combo {
-                        self.idx[next] = self.idx[next - 1] + 1;
-                        if self.idx[next] == self.vec.len() {
-                            continue 'inc;
-                        }
-                    }
-
-                    done = false;
-                    break;
-                }
-
-                if !done {
-                    return Some(self.combos());
-                }
-            }
-
-            self.combo += 1;
-            self.first = true;
-
-            if self.combo > self.vec.len() {
-                return None;
-            }
+        if self.current == 0 {
+            return None;
         }
+
+        let mut vec = Vec::new();
+        let mut num = self.current;
+        let mut index = 0;
+        self.current -= 1;
+        while num != 0 {
+            if (num & 1) == 1 {
+                vec.push(self.vec[index].clone());
+            }
+
+            index += 1;
+            num >>= 1;
+        }
+
+        Some(vec)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn combos_of_three_items() {
+        let v = vec!['A', 'B', 'C'];
+
+        let mut hs = HashSet::from([
+            vec!['A'],
+            vec!['B'],
+            vec!['C'],
+            vec!['A', 'B'],
+            vec!['A', 'C'],
+            vec!['B', 'C'],
+            vec!['A', 'B', 'C'],
+        ]);
+        for entry in v.combinations() {
+            assert!(hs.contains(&entry));
+            hs.remove(&entry);
+        }
+
+        assert!(hs.is_empty());
+    }
+
+    #[test]
+    fn single_entry_vec() {
+        let v = vec![1];
+        let mut iter = v.combinations();
+        assert_eq!(iter.next().unwrap(), vec![1]);
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn empty_vec() {
+        let v: Vec<i32> = Vec::new();
+        assert!(v.combinations().next().is_none());
     }
 }
