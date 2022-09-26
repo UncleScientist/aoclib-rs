50c5a6d57100230d389fb747ee588fc1c838af04
Uncle Scientist <unclescientist@gmail.com>
2022-09-26 07:44:59 -0400

    2015 day 24 part 2

--- src/bin/run-aoc/aoc2015/aoc2015_24.rs
+++ src/bin/run-aoc/aoc2015/aoc2015_24.rs
@@ -2,11 +2,34 @@ use aoclib::CombinationsOf;
 
 use crate::Runner;
 
-pub struct Aoc2015_24;
+pub struct Aoc2015_24 {
+    gifts: Vec<i64>,
+}
 
 impl Aoc2015_24 {
     pub fn new() -> Self {
-        Self {}
+        Self { gifts: Vec::new() }
+    }
+
+    fn qe_of_division(&self, divisor: i64) -> Option<i64> {
+        let target = self.gifts.iter().sum::<i64>() / divisor;
+
+        let mut done = false;
+        let mut qe = i64::MAX;
+        for n in 1..self.gifts.len() {
+            for combo in self.gifts.combinations_of(n) {
+                let sum: i64 = combo.iter().sum();
+                if sum == target {
+                    qe = qe.min(combo.iter().product());
+                    done = true;
+                }
+            }
+            if done {
+                return Some(qe);
+            }
+        }
+
+        None
     }
 }
 
@@ -15,33 +38,18 @@ impl Runner for Aoc2015_24 {
         (2015, 24)
     }
 
-    fn parse(&mut self) {}
-
-    fn part1(&mut self) -> Vec<String> {
-        let gifts: Vec<i64> = aoclib::read_lines("input/2015-24.txt")
+    fn parse(&mut self) {
+        self.gifts = aoclib::read_lines("input/2015-24.txt")
             .iter()
             .map(|x| x.parse::<i64>().unwrap())
             .collect();
-        let target = gifts.iter().sum::<i64>() / 3;
+    }
 
-        let mut done = false;
-        let mut qe = i64::MAX;
-        for n in 1..gifts.len() {
-            for combo in gifts.combinations_of(n) {
-                let sum: i64 = combo.iter().sum();
-                if sum == target {
-                    qe = qe.min(combo.iter().product());
-                    done = true;
-                }
-            }
-            if done {
-                break;
-            }
-        }
-        crate::output(qe)
+    fn part1(&mut self) -> Vec<String> {
+        crate::output(self.qe_of_division(3).unwrap())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        crate::output(self.qe_of_division(4).unwrap())
     }
 }
