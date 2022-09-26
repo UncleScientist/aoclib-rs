8b5b76a0f9ad426847e768dfcb57c3a0c1aea422
Uncle Scientist <unclescientist@gmail.com>
2022-09-26 08:17:53 -0400

    2015 day 25 -- all done

---
+++ src/bin/run-aoc/aoc2015/aoc2015_25.rs
@@ -0,0 +1,37 @@
+use crate::Runner;
+use aoclib::power_mod;
+
+pub struct Aoc2015_25;
+
+impl Aoc2015_25 {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    fn num_at_rc(row: u64, col: u64) -> u64 {
+        1 + (col + row - 1) * (col + row) / 2 - row
+    }
+}
+
+impl Runner for Aoc2015_25 {
+    fn name(&self) -> (usize, usize) {
+        (2015, 25)
+    }
+
+    fn parse(&mut self) {}
+
+    fn part1(&mut self) -> Vec<String> {
+        let row = 2978;
+        let col = 3083;
+        crate::output(power_mod(
+            20151125,
+            252533,
+            Self::num_at_rc(row, col) - 1,
+            33554393,
+        ))
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("ta-da!")
+    }
+}
--- src/bin/run-aoc/aoc2015/mod.rs
+++ src/bin/run-aoc/aoc2015/mod.rs
@@ -24,6 +24,7 @@ mod aoc2015_21;
 mod aoc2015_22;
 mod aoc2015_23;
 mod aoc2015_24;
+mod aoc2015_25;
 
 use aoc2015_01::*;
 use aoc2015_02::*;
@@ -49,6 +50,7 @@ use aoc2015_21::*;
 use aoc2015_22::*;
 use aoc2015_23::*;
 use aoc2015_24::*;
+use aoc2015_25::*;
 
 pub fn run_2015(which: Selector) {
     let mut day01 = Aoc2015_01::new();
@@ -75,12 +77,13 @@ pub fn run_2015(which: Selector) {
     let mut day22 = Aoc2015_22::new();
     let mut day23 = Aoc2015_23::new();
     let mut day24 = Aoc2015_24::new();
+    let mut day25 = Aoc2015_25::new();
 
     let mut days: Vec<&mut dyn Runner> = vec![
         &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
         &mut day08, &mut day09, &mut day10, &mut day11, &mut day12, &mut day13, &mut day14,
         &mut day15, &mut day16, &mut day17, &mut day18, &mut day19, &mut day20, &mut day21,
-        &mut day22, &mut day23, &mut day24,
+        &mut day22, &mut day23, &mut day24, &mut day25,
     ];
 
     match which {
