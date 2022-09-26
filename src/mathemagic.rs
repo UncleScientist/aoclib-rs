a0e83575d1d03a105f4b266eb49619411775cf28
Uncle Scientist <unclescientist@gmail.com>
2022-09-26 08:26:46 -0400

    make the arguments for power-mod generic

--- src/mathemagic.rs
+++ src/mathemagic.rs
@@ -1,13 +1,27 @@
-pub fn power_mod(start: u64, mut base: u64, mut exp: u64, modulus: u64) -> u64 {
+pub fn power_mod<T>(start: T, mut base: T, mut exp: T, modulus: T) -> T
+where
+    T: std::ops::Rem<Output = T>
+        + std::ops::RemAssign
+        + TryFrom<i32>
+        + Copy
+        + std::cmp::PartialOrd
+        + std::ops::ShrAssign
+        + std::ops::Mul<Output = T>,
+    <T as std::convert::TryFrom<i32>>::Error: std::fmt::Debug,
+{
     let mut result = start % modulus;
     base %= modulus;
 
-    while exp > 0 {
-        if exp & 1 == 1 {
+    let zero: T = 0_i32.try_into().unwrap();
+    let one: T = 1_i32.try_into().unwrap();
+    let two: T = 2_i32.try_into().unwrap();
+
+    while exp > zero {
+        if exp % two == one {
             result = (result * base) % modulus;
         }
 
-        exp >>= 1;
+        exp >>= one;
         base = (base * base) % modulus;
     }
 
