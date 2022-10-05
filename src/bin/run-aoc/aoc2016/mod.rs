fa71d9bb07ead7c9260fb81474863b4afd1a30ea
Uncle Scientist <unclescientist@gmail.com>
2022-10-05 07:51:45 -0400

    2016 day 4 part 1

--- src/bin/run-aoc/aoc2016/mod.rs
+++ src/bin/run-aoc/aoc2016/mod.rs
@@ -3,17 +3,20 @@ use crate::{Runner, Selector};
 mod aoc2016_01;
 mod aoc2016_02;
 mod aoc2016_03;
+mod aoc2016_04;
 
 use aoc2016_01::*;
 use aoc2016_02::*;
 use aoc2016_03::*;
+use aoc2016_04::*;
 
 pub fn run_2016(which: Selector) {
     let mut day01 = Aoc2016_01::new();
     let mut day02 = Aoc2016_02::new();
     let mut day03 = Aoc2016_03::new();
+    let mut day04 = Aoc2016_04::new();
 
-    let mut days: Vec<&mut dyn Runner> = vec![&mut day01, &mut day02, &mut day03];
+    let mut days: Vec<&mut dyn Runner> = vec![&mut day01, &mut day02, &mut day03, &mut day04];
 
     match which {
         Selector::Last => {
