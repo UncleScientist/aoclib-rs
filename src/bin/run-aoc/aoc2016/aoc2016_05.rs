ed5e846ea83a010424f2a768d8622e86616e047e
Uncle Scientist <unclescientist@gmail.com>
2022-10-05 08:30:54 -0400

    2016 day 5 part 2

--- src/bin/run-aoc/aoc2016/aoc2016_05.rs
+++ src/bin/run-aoc/aoc2016/aoc2016_05.rs
@@ -1,10 +1,18 @@
 use crate::Runner;
 
-pub struct Aoc2016_05;
+pub struct Aoc2016_05 {
+    prefix: String,
+    digest: Vec<md5::Digest>,
+    index: usize,
+}
 
 impl Aoc2016_05 {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            prefix: "ojvtpuvg".to_string(),
+            digest: Vec::new(),
+            index: 0,
+        }
     }
 }
 
@@ -17,29 +25,72 @@ impl Runner for Aoc2016_05 {
 
     fn part1(&mut self) -> Vec<String> {
         let mut result = "".to_string();
-        let prefix = "ojvtpuvg";
-        let mut count = 0;
-
-        for i in 0.. {
-            let digest = md5::compute(format!("{prefix}{i}"));
-
-            if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
-                let idx = (digest[2] & 0xf) as u8;
-                if idx < 10 {
-                    result.push((b'0' + idx) as char);
-                } else {
-                    result.push((b'a' + idx - 10) as char);
-                }
-                count += 1;
-                if count >= 8 {
-                    break;
-                }
+        while let Some(d) = self.next() {
+            result.push(val_to_char((d[2] & 0xf) as u8));
+            self.digest.push(d);
+            if result.len() == 8 {
+                println!("result is 8");
+                break;
             }
         }
+
         crate::output(result)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let mut ary = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+        let mut count = 0;
+        for d in self.digest.iter() {
+            let idx = (d[2] & 0xf) as usize;
+            if idx >= 8 {
+                continue;
+            }
+
+            let val = ((d[3] & 0xf0) >> 4) as u8;
+            if ary[idx] == ' ' {
+                ary[idx] = val_to_char(val);
+                count += 1;
+            }
+        }
+
+        while count < 8 {
+            let d = self.next().unwrap();
+            let idx = (d[2] & 0xf) as usize;
+            if idx >= 8 {
+                continue;
+            }
+
+            let val = ((d[3] & 0xf0) >> 4) as u8;
+            if ary[idx] == ' ' {
+                ary[idx] = val_to_char(val);
+                count += 1;
+            }
+        }
+
+        let result: String = ary.iter().collect();
+        crate::output(result)
+    }
+}
+
+impl Iterator for Aoc2016_05 {
+    type Item = md5::Digest;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        loop {
+            let digest = md5::compute(format!("{}{}", self.prefix, self.index));
+            self.index += 1;
+
+            if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
+                return Some(digest);
+            }
+        }
+    }
+}
+
+fn val_to_char(v: u8) -> char {
+    if v < 10 {
+        (b'0' + v) as char
+    } else {
+        (b'a' + v - 10) as char
     }
 }
