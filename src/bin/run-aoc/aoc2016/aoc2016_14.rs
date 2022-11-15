af0f962ddb171c075838d7bb0bf205b2b844907d
Uncle Scientist <unclescientist@gmail.com>
2022-11-15 08:34:07 -0500

    2016 day 14 part 2

--- src/bin/run-aoc/aoc2016/aoc2016_14.rs
+++ src/bin/run-aoc/aoc2016/aoc2016_14.rs
@@ -2,15 +2,11 @@ use std::collections::VecDeque;
 
 use crate::Runner;
 
-pub struct Aoc2016_14 {
-    salt: String,
-}
+pub struct Aoc2016_14;
 
 impl Aoc2016_14 {
     pub fn new() -> Self {
-        Self {
-            salt: "".to_string(),
-        }
+        Self {}
     }
 }
 
@@ -19,70 +15,78 @@ impl Runner for Aoc2016_14 {
         (2016, 14)
     }
 
-    fn parse(&mut self) {
-        self.salt = "ngcjuoqr".to_string();
-    }
+    fn parse(&mut self) {}
 
     fn part1(&mut self) -> Vec<String> {
-        let mut list = VecDeque::new();
-
-        for i in 0..1000 {
-            list.push_back(format!("{:x}", md5::compute(format!("{}{i}", self.salt))));
-        }
-        let mut next_hash = 1000;
-
-        let mut count = 0;
-        while count < 64 {
-            let cur = list.pop_front().unwrap();
-            list.push_back(format!(
-                "{:x}",
-                md5::compute(format!("{}{next_hash}", self.salt))
-            ));
-            next_hash += 1;
-
-            let mut last_char = '*';
-            let mut triple = 0;
-            for ch in cur.chars() {
-                if ch == last_char {
-                    triple += 1;
-                    if triple == 3 {
-                        break;
-                    }
-                } else {
-                    last_char = ch;
-                    triple = 1;
-                }
-            }
-
-            if triple != 3 {
-                continue;
-            }
-
-            let mut quintuple = 0;
-            'outer: for l in &list {
-                quintuple = 0;
-                for ch in l.chars() {
-                    if ch == last_char {
-                        quintuple += 1;
-                        if quintuple == 5 {
-                            break 'outer;
-                        }
-                    } else {
-                        quintuple = 0;
-                    }
-                }
-            }
-
-            if quintuple == 5 {
-                // println!("found at index {}", next_hash - 1001);
-                count += 1;
-            }
-        }
-
-        crate::output(next_hash - 1001)
+        crate::output(find_64th_key("ngcjuoqr".to_string(), 0))
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        crate::output(find_64th_key("ngcjuoqr".to_string(), 2016))
     }
+}
+
+fn stretch(unhashed: &String, rounds: usize) -> String {
+    let mut hash = format!("{:x}", md5::compute(unhashed));
+    for _ in 0..rounds {
+        hash = format!("{:x}", md5::compute(hash));
+    }
+
+    hash
+}
+
+fn find_64th_key(salt: String, stretch_count: usize) -> usize {
+    let mut list = VecDeque::new();
+
+    for i in 0..1000 {
+        list.push_back(stretch(&format!("{}{i}", salt), stretch_count));
+    }
+    let mut next_hash = 1000;
+
+    let mut count = 0;
+    while count < 64 {
+        let cur = list.pop_front().unwrap();
+        list.push_back(stretch(&format!("{}{next_hash}", salt), stretch_count));
+        next_hash += 1;
+
+        let mut last_char = '*';
+        let mut triple = 0;
+        for ch in cur.chars() {
+            if ch == last_char {
+                triple += 1;
+                if triple == 3 {
+                    break;
+                }
+            } else {
+                last_char = ch;
+                triple = 1;
+            }
+        }
+
+        if triple != 3 {
+            continue;
+        }
+
+        let mut quintuple = 0;
+        'outer: for l in &list {
+            quintuple = 0;
+            for ch in l.chars() {
+                if ch == last_char {
+                    quintuple += 1;
+                    if quintuple == 5 {
+                        break 'outer;
+                    }
+                } else {
+                    quintuple = 0;
+                }
+            }
+        }
+
+        if quintuple == 5 {
+            // println!("found at index {}", next_hash - 1001);
+            count += 1;
+        }
+    }
+
+    next_hash - 1001
 }
