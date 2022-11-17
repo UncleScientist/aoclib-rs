675dc4b37fc6db80e1cdf43eeb5c5c87fe984f53
Uncle Scientist <unclescientist@gmail.com>
2022-11-17 08:32:46 -0500

    2016 day 16 part 1

---
+++ src/bin/run-aoc/aoc2016/aoc2016_16.rs
@@ -0,0 +1,77 @@
+use crate::Runner;
+
+const PUZZLE_INPUT: &str = "11101000110010100";
+
+pub struct Aoc2016_16;
+
+impl Aoc2016_16 {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Runner for Aoc2016_16 {
+    fn name(&self) -> (usize, usize) {
+        (2016, 16)
+    }
+
+    fn parse(&mut self) {}
+
+    fn part1(&mut self) -> Vec<String> {
+        let scram = Scramble::new(PUZZLE_INPUT.to_string());
+        crate::output(scram.checksum(272))
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("unsolved")
+    }
+}
+
+struct Scramble {
+    input: String,
+}
+
+impl Scramble {
+    fn new(input: String) -> Self {
+        Self { input }
+    }
+
+    fn checksum(&self, len: usize) -> String {
+        let mut result = self.input.clone();
+
+        while result.len() < len {
+            let rev = result
+                .chars()
+                .rev()
+                .map(|ch| match ch {
+                    '0' => '1',
+                    '1' => '0',
+                    _ => panic!("invalid input"),
+                })
+                .collect::<String>();
+            result = format!("{result}0{rev}");
+        }
+
+        result.truncate(len);
+
+        while result.len() % 2 == 0 {
+            let slice = result.chars().collect::<Vec<char>>();
+            result = slice
+                .chunks(2)
+                .map(|pair| if pair[0] == pair[1] { '1' } else { '0' })
+                .collect::<String>();
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic_test() {
+        let scram = Scramble::new("10000".to_string());
+        assert_eq!("01100".to_string(), scram.checksum(20));
+    }
+}
