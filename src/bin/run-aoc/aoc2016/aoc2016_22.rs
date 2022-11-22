66d8aff429e49e3e2efd5741a3ef34da52f4985a
Uncle Scientist <unclescientist@gmail.com>
2022-11-22 08:15:21 -0500

    2016 day 22 part 2

--- src/bin/run-aoc/aoc2016/aoc2016_22.rs
+++ src/bin/run-aoc/aoc2016/aoc2016_22.rs
@@ -1,33 +1,28 @@
+use std::collections::HashMap;
+
 use crate::Runner;
 
+#[derive(Default)]
 pub struct Aoc2016_22 {
-    nodes: Vec<GridNode>,
+    nodes: HashMap<(usize, usize), GridNode>,
+    empty: (usize, usize),
+    largest: usize,
+    width: usize,
+    height: usize,
 }
 
 impl Aoc2016_22 {
     pub fn new() -> Self {
-        Self { nodes: Vec::new() }
+        Self {
+            ..Default::default()
+        }
     }
 }
 
 struct GridNode {
-    loc: (usize, usize),
     used: usize,
     avail: usize,
 }
-
-/*
-root@ebhq-gridcenter# df -h
-Filesystem              Size  Used  Avail  Use%
-/dev/grid/node-x0-y0     94T   67T    27T   71%
-/dev/grid/node-x0-y1     89T   67T    22T   75%
-/dev/grid/node-x0-y2     93T   69T    24T   74%
-/dev/grid/node-x0-y3     91T   66T    25T   72%
-/dev/grid/node-x0-y4     85T   69T    16T   81%
-/dev/grid/node-x0-y5     89T   64T    25T   71%
-/dev/grid/node-x0-y6     88T   70T    18T   79%
-/dev/grid/node-x0-y7     89T   65T    24T   73%
-*/
 
 impl Runner for Aoc2016_22 {
     fn name(&self) -> (usize, usize) {
@@ -44,25 +39,34 @@ impl Runner for Aoc2016_22 {
                 loc[0][1..].parse::<usize>().unwrap(),
                 loc[1][1..].parse::<usize>().unwrap(),
             );
+            self.width = self.width.max(loc.0);
+            self.height = self.height.max(loc.1);
             let used = col[2][0..col[2].len() - 1].parse::<usize>().unwrap();
+            if used == 0 {
+                self.empty = loc;
+            }
             let avail = col[3][0..col[3].len() - 1].parse::<usize>().unwrap();
-            self.nodes.push(GridNode { loc, used, avail });
+            self.largest = self.largest.max(avail);
+            self.nodes.insert(loc, GridNode { used, avail });
         }
-        println!("{}", self.nodes.len());
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let count = self.nodes.iter().filter(|a| a.used != 0).fold(0, |sum, a| {
-            sum + self
-                .nodes
-                .iter()
-                .filter(|b| a.loc != b.loc && a.used <= b.avail)
-                .count()
-        });
+        let count = self
+            .nodes
+            .iter()
+            .filter(|(_, a_node)| a_node.used != 0)
+            .fold(0, |sum, a| {
+                sum + self
+                    .nodes
+                    .iter()
+                    .filter(|b| a.0 != b.0 && a.1.used <= b.1.avail)
+                    .count()
+            });
         crate::output(count)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        crate::output(self.empty.0 + self.empty.1 + 6 * (self.width - 1) + 1)
     }
 }
