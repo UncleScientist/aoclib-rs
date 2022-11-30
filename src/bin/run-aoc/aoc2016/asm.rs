9953333f809a397f90ecd5b46ecec0acc5afb41a
Uncle Scientist <unclescientist@gmail.com>
2022-11-30 08:31:52 -0500

    2016 day 25

--- src/bin/run-aoc/aoc2016/asm.rs
+++ src/bin/run-aoc/aoc2016/asm.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
 #[derive(Debug, Clone)]
 pub enum Inst {
     Cpy(Source, Source),
@@ -5,6 +8,7 @@ pub enum Inst {
     Dec(Source),
     Jnz(Source, Source),
     Tgl(Source),
+    Out(Source),
 }
 
 impl Inst {
@@ -16,6 +20,7 @@ impl Inst {
             "jnz" => Inst::Jnz(Source::parse(tok[1]), Source::parse(tok[2])),
             "cpy" => Inst::Cpy(Source::parse(tok[1]), Source::parse(tok[2])),
             "tgl" => Inst::Tgl(Source::parse(tok[1])),
+            "out" => Inst::Out(Source::parse(tok[1])),
             _ => panic!("corrupted input file {tok:?}"),
         }
     }
@@ -26,6 +31,7 @@ impl Inst {
             Inst::Jnz(s, d) => Inst::Cpy(s.clone(), d.clone()),
             Inst::Cpy(s, d) => Inst::Jnz(s.clone(), d.clone()),
             Inst::Tgl(d) => Inst::Inc(d.clone()),
+            Inst::Out(d) => Inst::Inc(d.clone()),
         }
     }
 }
@@ -66,78 +72,139 @@ impl Register {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct Machine {
-    prog: Vec<Inst>,
+    ip: usize,
+    reg: [i32; 4],
     togg: Vec<bool>,
+    output: Option<i32>,
+    prog: Vec<Inst>,
+    state: HashSet<Machine>,
+    looped: bool,
 }
+
+impl Hash for Machine {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        self.ip.hash(state);
+        for r in self.reg {
+            r.hash(state);
+        }
+    }
+}
+
+impl PartialEq for Machine {
+    fn eq(&self, other: &Self) -> bool {
+        self.ip == other.ip && self.reg == other.reg
+    }
+}
+
+impl Eq for Machine {}
 
 impl Machine {
     pub fn push(&mut self, inst: Inst) {
         self.prog.push(inst);
     }
 
-    pub fn run(&mut self, initial_state: Vec<(Register, i32)>) -> i32 {
-        let mut reg: [i32; 4] = [0, 0, 0, 0];
-        let mut ip = 0;
+    pub fn looped(&self) -> bool {
+        self.looped
+    }
 
-        for i in initial_state {
-            reg[i.0 as usize] = i.1;
-        }
+    pub fn reset(&mut self, initial_state: Vec<(Register, i32)>) {
+        self.reg = [0, 0, 0, 0];
         self.togg = vec![false; self.prog.len()];
+        for i in initial_state {
+            self.reg[i.0 as usize] = i.1;
+        }
+        self.ip = 0;
+        self.state.clear();
+        self.looped = false;
+    }
 
-        while ip < self.prog.len() {
-            let oldip = ip;
-            ip += 1;
-            let inst = if self.togg[oldip] {
-                self.prog[oldip].toggle()
-            } else {
-                self.prog[oldip].clone()
-            };
-            match inst {
-                Inst::Inc(Source::Reg(r)) => reg[r as usize] += 1,
-                Inst::Dec(Source::Reg(r)) => reg[r as usize] -= 1,
-                Inst::Cpy(Source::Reg(r), Source::Reg(dest)) => {
-                    reg[dest as usize] = reg[r as usize]
-                }
-                Inst::Cpy(Source::Val(v), Source::Reg(dest)) => reg[dest as usize] = v,
-                Inst::Jnz(src, dest) => {
-                    let val = match src {
-                        Source::Reg(r) => reg[r as usize],
-                        Source::Val(v) => v,
-                    };
-                    let dest = match dest {
-                        Source::Reg(r) => reg[r as usize],
-                        Source::Val(v) => v,
-                    };
-                    if val != 0 {
-                        if dest < 0 {
-                            ip = oldip - (-dest) as usize;
-                        } else {
-                            ip = oldip + dest as usize;
-                        }
-                    }
-                }
-                Inst::Tgl(src) => {
-                    let dest = match src {
-                        Source::Reg(r) => reg[r as usize],
-                        Source::Val(v) => v,
-                    };
-                    let loc = if dest < 0 {
-                        oldip - (-dest) as usize
-                    } else {
-                        oldip + dest as usize
-                    };
-                    if loc < self.togg.len() {
-                        self.togg[loc] = !self.togg[loc];
-                    }
-                }
-                _ =>
-                    /* ignore */
-                    {}
+    pub fn run_to_output(&mut self) -> Option<i32> {
+        if !self.looped {
+            self.looped = !self.state.insert(self.clone());
+        }
+
+        self.output = None;
+
+        while self.ip < self.prog.len() {
+            self.step();
+            if self.output.is_some() {
+                return self.output;
             }
         }
 
-        reg[0]
+        None
+    }
+
+    pub fn run(&mut self, initial_state: Vec<(Register, i32)>) -> i32 {
+        self.reset(initial_state);
+
+        while self.ip < self.prog.len() {
+            self.step();
+        }
+
+        self.reg[0]
+    }
+
+    fn step(&mut self) {
+        let oldip = self.ip;
+        self.ip += 1;
+        let inst = if self.togg[oldip] {
+            self.prog[oldip].toggle()
+        } else {
+            self.prog[oldip].clone()
+        };
+        match inst {
+            Inst::Inc(Source::Reg(r)) => self.reg[r as usize] += 1,
+            Inst::Inc(Source::Val(_)) => { /* invalid */ }
+            Inst::Dec(Source::Reg(r)) => self.reg[r as usize] -= 1,
+            Inst::Dec(Source::Val(_)) => { /* invalid */ }
+            Inst::Cpy(Source::Reg(r), Source::Reg(dest)) => {
+                self.reg[dest as usize] = self.reg[r as usize]
+            }
+            Inst::Cpy(Source::Val(v), Source::Reg(dest)) => self.reg[dest as usize] = v,
+            Inst::Cpy(_, Source::Val(_)) => { /* invalid */ }
+            Inst::Jnz(src, dest) => {
+                let val = match src {
+                    Source::Reg(r) => self.reg[r as usize],
+                    Source::Val(v) => v,
+                };
+                let dest = match dest {
+                    Source::Reg(r) => self.reg[r as usize],
+                    Source::Val(v) => v,
+                };
+                if val != 0 {
+                    if dest < 0 {
+                        self.ip = oldip - (-dest) as usize;
+                    } else {
+                        self.ip = oldip + dest as usize;
+                    }
+                }
+            }
+            Inst::Tgl(src) => {
+                let dest = match src {
+                    Source::Reg(r) => self.reg[r as usize],
+                    Source::Val(v) => v,
+                };
+                let loc = if dest < 0 {
+                    oldip - (-dest) as usize
+                } else {
+                    oldip + dest as usize
+                };
+                if loc < self.togg.len() {
+                    self.togg[loc] = !self.togg[loc];
+                }
+            }
+            Inst::Out(src) => {
+                self.output = Some(match src {
+                    Source::Reg(r) => self.reg[r as usize],
+                    Source::Val(v) => v,
+                })
+            }
+        }
     }
 }
