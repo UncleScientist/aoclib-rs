e1148f653506ea2c9ab10ed708e3cd6cae49a41a
Uncle Scientist <unclescientist@gmail.com>
2022-11-30 08:31:52 -0500

    2016 day 25

---
+++ src/bin/run-aoc/aoc2016/aoc2016_25.rs
@@ -0,0 +1,52 @@
+use crate::aoc2016::asm::*;
+use crate::Runner;
+
+#[derive(Default)]
+pub struct Aoc2016_25 {
+    vm: Machine,
+}
+
+impl Aoc2016_25 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2016_25 {
+    fn name(&self) -> (usize, usize) {
+        (2016, 25)
+    }
+
+    fn parse(&mut self) {
+        let lines = aoclib::read_lines("input/2016-25.txt");
+        for l in lines {
+            self.vm.push(Inst::parse(&l));
+        }
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        'next_number: for init in 1i32.. {
+            self.vm.reset(vec![(Register::A, init)]);
+            let mut last_output = 1;
+            let answer = loop {
+                if let Some(next_output) = self.vm.run_to_output() {
+                    if next_output == last_output {
+                        continue 'next_number;
+                    }
+                    last_output = next_output;
+                    if self.vm.looped() {
+                        break init;
+                    }
+                } else {
+                    continue 'next_number;
+                }
+            };
+            return crate::output(answer);
+        }
+        crate::output("failed")
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("unneeded")
+    }
+}
--- src/bin/run-aoc/aoc2016/mod.rs
+++ src/bin/run-aoc/aoc2016/mod.rs
@@ -26,6 +26,7 @@ mod aoc2016_21;
 mod aoc2016_22;
 mod aoc2016_23;
 mod aoc2016_24;
+mod aoc2016_25;
 
 use aoc2016_01::*;
 use aoc2016_02::*;
@@ -51,6 +52,7 @@ use aoc2016_21::*;
 use aoc2016_22::*;
 use aoc2016_23::*;
 use aoc2016_24::*;
+use aoc2016_25::*;
 
 pub fn run_2016(which: Selector) {
     let mut day01 = Aoc2016_01::new();
@@ -77,12 +79,13 @@ pub fn run_2016(which: Selector) {
     let mut day22 = Aoc2016_22::new();
     let mut day23 = Aoc2016_23::new();
     let mut day24 = Aoc2016_24::new();
+    let mut day25 = Aoc2016_25::new();
 
     let mut days: Vec<&mut dyn Runner> = vec![
         &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
         &mut day08, &mut day09, &mut day10, &mut day11, &mut day12, &mut day13, &mut day14,
         &mut day15, &mut day16, &mut day17, &mut day18, &mut day19, &mut day20, &mut day21,
-        &mut day22, &mut day23, &mut day24,
+        &mut day22, &mut day23, &mut day24, &mut day25,
     ];
 
     match which {
