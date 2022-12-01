c0052af176129eded49e5f1362fee77709445625
Uncle Scientist <unclescientist@gmail.com>
2022-12-01 08:42:13 -0500

    2017 day 1

--- src/bin/run-aoc/aoc2017/aoc2017_01.rs
+++ src/bin/run-aoc/aoc2017/aoc2017_01.rs
@@ -1,10 +1,13 @@
 use crate::Runner;
 
-pub struct Aoc2017_01;
+#[derive(Default)]
+pub struct Aoc2017_01 {
+    nums: Vec<u64>,
+}
 
 impl Aoc2017_01 {
     pub fn new() -> Self {
-        Self {}
+        Self::default()
     }
 }
 
@@ -13,13 +16,31 @@ impl Runner for Aoc2017_01 {
         (2017, 1)
     }
 
-    fn parse(&mut self) {}
+    fn parse(&mut self) {
+        self.nums = aoclib::read_single_line("input/2017-01.txt")
+            .iter()
+            .map(|&ch| ((ch as u8) - b'0') as u64)
+            .collect();
+        self.nums.push(self.nums[0]);
+    }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let sum = self
+            .nums
+            .windows(2)
+            .filter(|p| p[0] == p[1])
+            .map(|p| p[0])
+            .sum::<u64>();
+        crate::output(sum)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let len = self.nums.len();
+        let sum = 2
+            * (0..len / 2)
+                .filter(|&p| self.nums[p] == self.nums[p + (len / 2)])
+                .map(|p| self.nums[p])
+                .sum::<u64>();
+        crate::output(sum)
     }
 }
