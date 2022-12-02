5fcdae671932e4213e38861ad5f8c225f366dc24
Uncle Scientist <unclescientist@gmail.com>
2022-12-02 08:40:27 -0500

    2017 day 3 part 2

--- src/bin/run-aoc/aoc2017/aoc2017_03.rs
+++ src/bin/run-aoc/aoc2017/aoc2017_03.rs
@@ -2,6 +2,8 @@ use crate::Runner;
 
 use std::collections::HashMap;
 
+const PUZZLE_INPUT: i32 = 312051;
+
 #[derive(Default)]
 pub struct Aoc2017_03 {
     // insert items here (or not, i'm not the boss of you)
@@ -21,7 +23,7 @@ impl Runner for Aoc2017_03 {
     fn parse(&mut self) {}
 
     fn part1(&mut self) -> Vec<String> {
-        let mut hs = HashMap::new();
+        let mut hm = HashMap::new();
         let left: HashMap<(i32, i32), (i32, i32)> = HashMap::from_iter([
             ((1, 0), (0, 1)),
             ((0, 1), (-1, 0)),
@@ -33,15 +35,15 @@ impl Runner for Aoc2017_03 {
         let mut y = 0;
         let mut cur_num = 1;
         let mut dir = (1, 0);
-        hs.insert((x, y), cur_num);
+        hm.insert((x, y), cur_num);
 
-        while cur_num < 312051 {
+        while cur_num < PUZZLE_INPUT {
             cur_num += 1;
             x += dir.0;
             y += dir.1;
-            hs.insert((x, y), cur_num);
+            hm.insert((x, y), cur_num);
             let left_dir = left.get(&dir).unwrap();
-            if !hs.contains_key(&(x + left_dir.0, y + left_dir.1)) {
+            if !hm.contains_key(&(x + left_dir.0, y + left_dir.1)) {
                 dir = *left_dir;
             }
         }
@@ -50,6 +52,47 @@ impl Runner for Aoc2017_03 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let surround = vec![
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ];
+        let left: HashMap<(i32, i32), (i32, i32)> = HashMap::from_iter([
+            ((1, 0), (0, 1)),
+            ((0, 1), (-1, 0)),
+            ((-1, 0), (0, -1)),
+            ((0, -1), (1, 0)),
+        ]);
+
+        let mut hm = HashMap::new();
+        let mut x = 0;
+        let mut y = 0;
+        let mut cur_num = 1;
+        let mut dir = (1, 0);
+        hm.insert((x, y), cur_num);
+
+        while cur_num < PUZZLE_INPUT {
+            cur_num += 1;
+            x += dir.0;
+            y += dir.1;
+            let sum: i32 = surround
+                .iter()
+                .map(|coord| hm.get(&(x + coord.0, y + coord.1)).unwrap_or(&0))
+                .sum();
+            hm.insert((x, y), sum);
+            if sum > PUZZLE_INPUT {
+                return crate::output(sum);
+            }
+            let left_dir = left.get(&dir).unwrap();
+            if !hm.contains_key(&(x + left_dir.0, y + left_dir.1)) {
+                dir = *left_dir;
+            }
+        }
+        crate::output("FAILED")
     }
 }
