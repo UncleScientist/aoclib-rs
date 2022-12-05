47180d74352f798d3de2f368b23ff672c52df055
Uncle Scientist <unclescientist@gmail.com>
2022-12-05 08:31:53 -0500

    2017 day 6 part 1

--- src/bin/run-aoc/aoc2017/mod.rs
+++ src/bin/run-aoc/aoc2017/mod.rs
@@ -5,12 +5,14 @@ mod aoc2017_02;
 mod aoc2017_03;
 mod aoc2017_04;
 mod aoc2017_05;
+mod aoc2017_06;
 
 use aoc2017_01::*;
 use aoc2017_02::*;
 use aoc2017_03::*;
 use aoc2017_04::*;
 use aoc2017_05::*;
+use aoc2017_06::*;
 
 pub fn run_2017(which: Selector) {
     let mut day01 = Aoc2017_01::new();
@@ -18,9 +20,11 @@ pub fn run_2017(which: Selector) {
     let mut day03 = Aoc2017_03::new();
     let mut day04 = Aoc2017_04::new();
     let mut day05 = Aoc2017_05::new();
+    let mut day06 = Aoc2017_06::new();
 
-    let mut days: Vec<&mut dyn Runner> =
-        vec![&mut day01, &mut day02, &mut day03, &mut day04, &mut day05];
+    let mut days: Vec<&mut dyn Runner> = vec![
+        &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06,
+    ];
 
     match which {
         Selector::Last => {
