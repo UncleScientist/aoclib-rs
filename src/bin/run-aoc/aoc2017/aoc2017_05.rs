76a703010add1f82b8dba48214a1127b0b53e15e
Uncle Scientist <unclescientist@gmail.com>
2022-12-05 08:10:13 -0500

    2017 day 5 part 1

---
+++ src/bin/run-aoc/aoc2017/aoc2017_05.rs
@@ -0,0 +1,40 @@
+use crate::Runner;
+
+#[derive(Default)]
+pub struct Aoc2017_05 {
+    jumps: Vec<i32>,
+}
+
+impl Aoc2017_05 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2017_05 {
+    fn name(&self) -> (usize, usize) {
+        (2017, 5)
+    }
+
+    fn parse(&mut self) {
+        self.jumps = aoclib::read_numbers("input/2017-05.txt");
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        let mut jumps = self.jumps.clone();
+
+        let mut ip = 0i32;
+        let mut count = 0;
+        while ip < jumps.len() as i32 {
+            let offset = jumps[ip as usize];
+            jumps[ip as usize] += 1;
+            ip += offset;
+            count += 1;
+        }
+        crate::output(count)
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("unsolved")
+    }
+}
