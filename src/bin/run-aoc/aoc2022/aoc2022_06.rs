4adb0939afb1de0d9c62587c6b9b494cb663fadf
Uncle Scientist <unclescientist@gmail.com>
2022-12-06 07:49:13 -0500

    2022 day 6 part 2

--- src/bin/run-aoc/aoc2022/aoc2022_06.rs
+++ src/bin/run-aoc/aoc2022/aoc2022_06.rs
@@ -24,26 +24,66 @@ impl Runner for Aoc2022_06 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output(first_marker(&self.input))
+        crate::output(sliding_window(&self.input, 4).unwrap())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        crate::output(sliding_window(&self.input, 14).unwrap())
     }
 }
 
-fn first_marker(stream: &[char]) -> usize {
-    'next_window: for (index, window) in stream.windows(4).enumerate() {
-        for i in 0..3 {
-            for j in i + 1..4 {
+fn _first_marker(stream: &[char], width: usize) -> Option<usize> {
+    'next_window: for (index, window) in stream.windows(width).enumerate() {
+        for i in 0..width - 1 {
+            for j in i + 1..width {
                 if window[i] == window[j] {
                     continue 'next_window;
                 }
             }
         }
 
-        return index + 4;
+        return Some(index + width);
     }
 
-    0
+    None
+}
+
+fn sliding_window(stream: &[char], width: usize) -> Option<usize> {
+    let mut start = width + 1;
+    let mut counts = vec![0; 26];
+    let mut dups = 0;
+
+    let mut front = stream.iter();
+    let mut back = stream.iter();
+
+    for _ in 0..width {
+        let index = ((*front.next().unwrap() as u8) - b'a') as usize;
+        if counts[index] == 1 {
+            dups += 1;
+        }
+        counts[index] += 1;
+    }
+
+    while start < stream.len() {
+        let first = ((*back.next().unwrap() as u8) - b'a') as usize;
+        let last = ((*front.next().unwrap() as u8) - b'a') as usize;
+
+        counts[first] -= 1;
+        if counts[first] == 1 {
+            dups -= 1;
+        }
+
+        counts[last] += 1;
+        if counts[last] == 2 {
+            dups += 1;
+        }
+
+        if dups == 0 {
+            return Some(start);
+        }
+
+        start += 1;
+    }
+
+    None
 }
