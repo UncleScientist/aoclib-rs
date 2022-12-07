29d6325f674b60013d76fba7df963db61a1d4fd2
Uncle Scientist <unclescientist@gmail.com>
2022-12-07 08:31:24 -0500

    2017 day 7 part 2

--- src/bin/run-aoc/aoc2017/aoc2017_07.rs
+++ src/bin/run-aoc/aoc2017/aoc2017_07.rs
@@ -5,8 +5,7 @@ use crate::Runner;
 #[derive(Default)]
 pub struct Aoc2017_07 {
     tree: HashMap<String, Node>,
-    lhs: HashSet<String>,
-    rhs: HashSet<String>,
+    root_name: String,
 }
 
 impl Aoc2017_07 {
@@ -21,40 +20,75 @@ impl Runner for Aoc2017_07 {
     }
 
     fn parse(&mut self) {
+        let mut lhs = HashSet::new();
+        let mut rhs = HashSet::new();
+
         for line in aoclib::read_lines("input/2017-07.txt") {
             let words = line.split(" -> ").collect::<Vec<&str>>();
             let (left, wstr) = words[0].split_once(' ').unwrap();
             let weight = wstr[1..wstr.len() - 1].parse::<usize>().unwrap();
             let mut node = Node {
-                _name: left.to_string(),
-                _weight: weight,
+                weight,
                 child: Vec::new(),
             };
 
-            self.lhs.insert(left.to_string());
+            lhs.insert(left.to_string());
 
             if words.len() > 1 {
                 for right in words[1].split(", ") {
                     node.child.push(right.to_string());
-                    self.rhs.insert(right.to_string());
+                    rhs.insert(right.to_string());
                 }
             }
             self.tree.insert(left.to_string(), node);
         }
+        self.root_name = lhs.difference(&rhs).take(1).next().unwrap().clone();
     }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output(self.lhs.difference(&self.rhs).take(1).next().unwrap())
+        crate::output(&self.root_name)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let mut ptr = self.tree.get(&self.root_name).unwrap();
+        let mut diff = 0i32;
+
+        let bad_node = loop {
+            let mut weights = ptr
+                .child
+                .iter()
+                .map(|c| (c, get_weight(&self.tree, c)))
+                .collect::<Vec<(_, _)>>();
+            weights.sort_by(|a, b| a.1.cmp(&b.1));
+            let last = weights.len() - 1;
+            if weights[0].1 == weights[last].1 {
+                // it's balanced from this point up
+                break ptr;
+            } else if weights[0].1 == weights[1].1 {
+                diff = (weights[0].1 as i32) - (weights[last].1 as i32);
+                ptr = self.tree.get(weights[last].0).unwrap();
+            } else {
+                diff = (weights[1].1 as i32) - (weights[0].1 as i32);
+                ptr = self.tree.get(weights[0].0).unwrap();
+            }
+        };
+
+        crate::output((bad_node.weight as i32) + diff)
     }
 }
 
 #[derive(Debug)]
 struct Node {
-    _name: String,
-    _weight: usize,
+    weight: usize,
     child: Vec<String>,
+}
+
+fn get_weight(root: &HashMap<String, Node>, name: &str) -> usize {
+    let node = root.get(name).unwrap();
+    node.weight
+        + node
+            .child
+            .iter()
+            .map(|n| get_weight(root, n))
+            .sum::<usize>()
 }
