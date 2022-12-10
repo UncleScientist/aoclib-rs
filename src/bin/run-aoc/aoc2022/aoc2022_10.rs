8d7e58e0be6ffbdaed16bcac9889d289b53a7356
Uncle Scientist <unclescientist@gmail.com>
2022-12-10 08:08:57 -0500

    2022 day 10 part 2

--- src/bin/run-aoc/aoc2022/aoc2022_10.rs
+++ src/bin/run-aoc/aoc2022/aoc2022_10.rs
@@ -3,6 +3,7 @@ use crate::Runner;
 #[derive(Default)]
 pub struct Aoc2022_10 {
     stmt: Vec<Instruction>,
+    machine: Machine,
 }
 
 impl Aoc2022_10 {
@@ -32,35 +33,37 @@ impl Runner for Aoc2022_10 {
 
     fn part1(&mut self) -> Vec<String> {
         let mut strength = 0;
-        let mut xreg = 1;
-        let mut cycle = 0;
         let mut check = 20;
+
+        self.machine.init();
 
         for s in &self.stmt {
             match s {
                 Instruction::Noop => {
-                    cycle += 1;
-                    if cycle >= check {
-                        strength += cycle * xreg;
+                    self.machine.noop();
+                    if self.machine.cycle >= check {
+                        strength += check * self.machine.xreg;
                         check += 40;
                     }
                 }
                 Instruction::AddX(val) => {
-                    cycle += 2;
-                    if cycle >= check {
-                        strength += check * xreg;
+                    let prev_x = self.machine.xreg;
+                    self.machine.addx(*val);
+                    if self.machine.cycle >= check {
+                        strength += check * prev_x;
                         check += 40;
                     }
-
-                    xreg += val;
                 }
             }
         }
+
+        self.machine.noop();
+
         crate::output(strength)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        self.machine.crt.clone()
     }
 }
 
@@ -68,4 +71,44 @@ impl Runner for Aoc2022_10 {
 enum Instruction {
     AddX(i32),
     Noop,
+}
+
+#[derive(Default)]
+struct Machine {
+    xreg: i32,
+    cycle: i32,
+    scanline: String,
+    crt: Vec<String>,
+}
+
+impl Machine {
+    fn init(&mut self) {
+        self.xreg = 1;
+        self.cycle = 0;
+        self.scanline = "".to_string();
+        self.crt.clear();
+    }
+
+    fn noop(&mut self) {
+        self.cycle();
+    }
+
+    fn addx(&mut self, delta: i32) {
+        self.cycle();
+        self.cycle();
+        self.xreg += delta;
+    }
+
+    fn cycle(&mut self) {
+        if self.cycle % 40 == 0 && !self.scanline.is_empty() {
+            self.crt.push(self.scanline.clone());
+            self.scanline.clear();
+        }
+        if self.cycle % 40 < self.xreg - 1 || self.cycle % 40 > self.xreg + 1 {
+            self.scanline.push('.')
+        } else {
+            self.scanline.push('#');
+        }
+        self.cycle += 1;
+    }
 }
