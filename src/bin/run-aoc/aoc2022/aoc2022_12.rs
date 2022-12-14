d00a3f372704e166529b1ab85ceb5db88563df97
Uncle Scientist <unclescientist@gmail.com>
2022-12-14 08:41:08 -0500

    BFS for 2022 day 12

--- src/bin/run-aoc/aoc2022/aoc2022_12.rs
+++ src/bin/run-aoc/aoc2022/aoc2022_12.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashSet, VecDeque};
 
 use crate::Runner;
 
@@ -43,75 +43,67 @@ impl Runner for Aoc2022_12 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output(self.find_shortest(self.start).unwrap())
+        crate::output(self.bfs(self.start, false).unwrap())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        let mut startpoints = Vec::new();
-        for (row, line) in self.grid.iter().enumerate() {
-            for (col, ch) in line.iter().enumerate() {
-                if *ch == b'a' {
-                    startpoints.push((row, col));
-                }
-            }
-        }
-
-        crate::output(
-            startpoints
-                .iter()
-                .filter_map(|p| self.find_shortest(*p))
-                .min()
-                .unwrap(),
-        )
+        crate::output(self.bfs(self.end, true).unwrap())
     }
 }
 
 impl Aoc2022_12 {
     const DIR: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
 
-    fn get_surrounding_points(&self, pos: (usize, usize)) -> Vec<(usize, usize)> {
+    fn get_surrounding_points(
+        &self,
+        pos: (usize, usize),
+        f: &dyn Fn((usize, usize), u8) -> bool,
+    ) -> Vec<(usize, usize)> {
         let ipos = (pos.0 as i32, pos.1 as i32);
         let width = self.width as i32;
         let height = self.height as i32;
+        let cur_elevation = self.grid[pos.0][pos.1];
+
         Self::DIR
             .iter()
             .map(|d| (ipos.0 + d.0, ipos.1 + d.1))
             .filter(|pos| pos.0 >= 0 && pos.1 >= 0 && pos.0 < height && pos.1 < width)
             .map(|pos| (pos.0 as usize, pos.1 as usize))
+            .filter(|pos| f(*pos, cur_elevation))
             .collect()
     }
 
-    fn find_shortest(&self, start_point: (usize, usize)) -> Option<usize> {
-        let mut to_visit = Vec::new();
+    fn bfs(&self, start_point: (usize, usize), backwards: bool) -> Option<usize> {
+        let mut visited = HashSet::new();
+        let mut queue = VecDeque::new();
 
-        let mut shortest: HashMap<(usize, usize), usize> = HashMap::new();
-        shortest.insert(start_point, 0);
+        visited.insert(start_point);
+        queue.push_back((start_point, 0));
 
-        to_visit.extend(self.get_surrounding_points(start_point));
-        while let Some(loc) = to_visit.pop() {
-            let cur_elevation = self.grid[loc.0][loc.1];
-            let points = self.get_surrounding_points(loc);
-            let valid = points
-                .iter()
-                .filter(|pos| self.grid[pos.0][pos.1] + 1 >= cur_elevation)
-                .copied()
-                .collect::<Vec<(usize, usize)>>();
+        while let Some(loc) = queue.pop_front() {
+            let valid = if backwards {
+                self.get_surrounding_points(loc.0, &|pos: (usize, usize), elevation: u8| {
+                    self.grid[pos.0][pos.1] >= elevation - 1
+                })
+            } else {
+                self.get_surrounding_points(loc.0, &|pos: (usize, usize), elevation: u8| {
+                    self.grid[pos.0][pos.1] <= elevation + 1
+                })
+            };
 
-            let new_path_dist = valid.iter().filter_map(|pos| shortest.get(pos)).min();
+            for v in valid {
+                if !visited.insert(v) {
+                    continue;
+                }
 
-            if new_path_dist.is_none() {
-                continue;
-            }
+                if (backwards && self.grid[v.0][v.1] == b'a') || (!backwards && v == self.end) {
+                    return Some(loc.1 + 1);
+                }
 
-            let new_path_dist = new_path_dist.unwrap() + 1;
-
-            let cur_path_dist = shortest.entry(loc).or_insert(usize::MAX);
-            if *cur_path_dist > new_path_dist {
-                *cur_path_dist = new_path_dist;
-                to_visit.extend(points.iter());
+                queue.push_back((v, loc.1 + 1));
             }
         }
 
-        shortest.get(&self.end).copied()
+        None
     }
 }
