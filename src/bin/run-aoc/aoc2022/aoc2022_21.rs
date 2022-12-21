4028277d224b2356c77099dade84e0a41e23008f
Uncle Scientist <unclescientist@gmail.com>
2022-12-21 09:46:23 -0500

    2022 day 21 part 2

--- src/bin/run-aoc/aoc2022/aoc2022_21.rs
+++ src/bin/run-aoc/aoc2022/aoc2022_21.rs
@@ -46,7 +46,60 @@ impl Runner for Aoc2022_21 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let root = "root".to_string();
+        let path = find_human(&root, &self.tree).unwrap();
+        let path = path.iter().rev().copied().collect::<Vec<_>>();
+
+        let (left, right) = match self.tree.get(&root).unwrap() {
+            Monkey::Value(_) => panic!("root monkey has no data"),
+            Monkey::Binary(left, _, right) => (left, right),
+        };
+
+        let correct_val = if left == path[1] {
+            eval(right, &self.tree)
+        } else {
+            eval(left, &self.tree)
+        };
+
+        crate::output(find_adjustment(&path, 1, &self.tree, correct_val))
+    }
+}
+
+// left + right = cv
+// left - right = cv
+// left * right = cv
+// left / right = cv
+
+fn find_adjustment(
+    path: &Vec<&String>,
+    index: usize,
+    tree: &HashMap<String, Monkey>,
+    cv: i64,
+) -> i64 {
+    use Operation::*;
+
+    match tree.get(path[index]).unwrap() {
+        Monkey::Value(_) => cv,
+        Monkey::Binary(l, op, r) => {
+            let left = eval(l, tree);
+            let right = eval(r, tree);
+            let new_cv = if l == path[index + 1] {
+                match op {
+                    Addition => cv - right,
+                    Subtraction => cv + right,
+                    Multiplication => cv / right,
+                    Division => cv * right,
+                }
+            } else {
+                match op {
+                    Addition => cv - left,
+                    Subtraction => left - cv,
+                    Multiplication => cv / left,
+                    Division => left / cv,
+                }
+            };
+            find_adjustment(path, index + 1, tree, new_cv)
+        }
     }
 }
 
@@ -65,6 +118,25 @@ fn eval(start: &String, tree: &HashMap<String, Monkey>) -> i64 {
             }
         }
     }
+}
+
+fn find_human<'a>(loc: &'a String, tree: &'a HashMap<String, Monkey>) -> Option<Vec<&'a String>> {
+    if loc == "humn" {
+        return Some(vec![loc]);
+    }
+
+    if let Some(Monkey::Binary(l, _, r)) = tree.get(loc) {
+        if let Some(mut vec) = find_human(l, tree) {
+            vec.push(loc);
+            return Some(vec);
+        }
+        if let Some(mut vec) = find_human(r, tree) {
+            vec.push(loc);
+            return Some(vec);
+        }
+    }
+
+    None
 }
 
 #[derive(Debug)]
