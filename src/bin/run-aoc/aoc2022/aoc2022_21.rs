988e36ba1a86ac7055d932158b7c6b3308abedf7
Uncle Scientist <unclescientist@gmail.com>
2022-12-21 09:18:19 -0500

    2022 day 21 part 1

---
+++ src/bin/run-aoc/aoc2022/aoc2022_21.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+
+use crate::Runner;
+
+#[derive(Default)]
+pub struct Aoc2022_21 {
+    tree: HashMap<String, Monkey>,
+}
+
+impl Aoc2022_21 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2022_21 {
+    fn name(&self) -> (usize, usize) {
+        (2022, 21)
+    }
+
+    fn parse(&mut self) {
+        let _lines = aoclib::read_lines("test-input.txt");
+        let lines = aoclib::read_lines("input/2022-21.txt");
+
+        for line in lines {
+            let (left, right) = line.split_once(": ").unwrap();
+
+            if let Ok(num) = right.parse::<i64>() {
+                self.tree.insert(left.to_string(), Monkey::Value(num));
+            } else {
+                let expr = right.split_whitespace().collect::<Vec<_>>();
+                self.tree.insert(
+                    left.to_string(),
+                    Monkey::Binary(
+                        expr[0].to_string(),
+                        Operation::parse(expr[1].chars().next().unwrap()),
+                        expr[2].to_string(),
+                    ),
+                );
+            }
+        }
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        crate::output(eval(&"root".to_string(), &self.tree))
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("unsolved")
+    }
+}
+
+fn eval(start: &String, tree: &HashMap<String, Monkey>) -> i64 {
+    use Operation::*;
+    match tree.get(start).unwrap() {
+        Monkey::Value(v) => *v,
+        Monkey::Binary(l, op, r) => {
+            let l = eval(l, tree);
+            let r = eval(r, tree);
+            match op {
+                Addition => l + r,
+                Subtraction => l - r,
+                Multiplication => l * r,
+                Division => l / r,
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Monkey {
+    Value(i64),
+    Binary(String, Operation, String),
+}
+
+#[derive(Debug)]
+enum Operation {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
+
+impl Operation {
+    fn parse(ch: char) -> Self {
+        match ch {
+            '+' => Self::Addition,
+            '-' => Self::Subtraction,
+            '*' => Self::Multiplication,
+            '/' => Self::Division,
+            _ => panic!("not an operator: '{ch}'"),
+        }
+    }
+}
