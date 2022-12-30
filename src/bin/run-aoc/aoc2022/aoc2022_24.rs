6932b9d88b96694f275cf9bb0f6ada6a4c183a09
Uncle Scientist <unclescientist@gmail.com>
2022-12-30 09:37:12 -0500

    2022 day 24 part 2

--- src/bin/run-aoc/aoc2022/aoc2022_24.rs
+++ src/bin/run-aoc/aoc2022/aoc2022_24.rs
@@ -1,3 +1,5 @@
+use std::hash::{Hash, Hasher};
+
 use aoclib::{dijkstra_search, DijkstraSearch};
 
 use crate::Runner;
@@ -5,6 +7,8 @@ use crate::Runner;
 #[derive(Default)]
 pub struct Aoc2022_24 {
     valley: Blizzard,
+    first_trip_map_idx: usize,
+    first_trip_time: usize,
 }
 
 impl Aoc2022_24 {
@@ -27,37 +31,109 @@ impl Runner for Aoc2022_24 {
         self.valley = Blizzard::new(width, height);
 
         let mut idx = 0;
+        let mut map = vec![0; (width * height) as usize];
         for line in lines {
             for c in line.chars() {
                 match c {
-                    '#' => self.valley.map[idx] = Blizzard::WALL,
-                    '^' => self.valley.map[idx] = Blizzard::UP,
-                    'v' => self.valley.map[idx] = Blizzard::DOWN,
-                    '<' => self.valley.map[idx] = Blizzard::LEFT,
-                    '>' => self.valley.map[idx] = Blizzard::RIGHT,
+                    '#' => map[idx] = Blizzard::WALL,
+                    '^' => map[idx] = Blizzard::UP,
+                    'v' => map[idx] = Blizzard::DOWN,
+                    '<' => map[idx] = Blizzard::LEFT,
+                    '>' => map[idx] = Blizzard::RIGHT,
                     '.' => {}
                     _ => panic!("unknown char '{c}'"),
                 };
                 idx += 1;
             }
         }
+        self.valley.maps.push(map);
+
+        self.valley.generate_maps();
     }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output(dijkstra_search(&self.valley).unwrap().1)
+        let state = State::new(&self.valley);
+
+        let (result, time) = dijkstra_search(&state).unwrap();
+        self.first_trip_map_idx = result.map_idx;
+        self.first_trip_time = time;
+        crate::output(time)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let mut return_trip = State::new(&self.valley);
+        return_trip.player_pos = (self.valley.height - 1, self.valley.width - 2);
+        return_trip.map_idx = self.first_trip_map_idx;
+        return_trip.find_start = true;
+
+        let (mut back_to_end, return_time) = dijkstra_search(&return_trip).unwrap();
+        back_to_end.find_start = false;
+
+        let final_time = dijkstra_search(&back_to_end).unwrap().1;
+
+        crate::output(self.first_trip_time + return_time + final_time)
     }
 }
 
-#[derive(Default, Clone, PartialEq, Eq, Hash)]
+#[derive(Default, Clone)]
 struct Blizzard {
     width: i32,
     height: i32,
-    map: Vec<u8>,
+    maps: Vec<Vec<u8>>,
+}
+
+#[derive(Clone)]
+struct State<'a> {
     player_pos: (i32, i32),
+    map_idx: usize,
+    find_start: bool,
+    bliz: &'a Blizzard,
+}
+
+impl<'a> PartialEq for State<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.player_pos == other.player_pos
+            && self.map_idx == other.map_idx
+            && self.find_start == other.find_start
+    }
+}
+
+impl<'a> Eq for State<'a> {}
+
+impl<'a> Hash for State<'a> {
+    fn hash<T: Hasher>(&self, state: &mut T) {
+        self.player_pos.hash(state);
+        self.map_idx.hash(state);
+        self.find_start.hash(state);
+    }
+}
+
+impl<'a> State<'a> {
+    fn new(bliz: &'a Blizzard) -> Self {
+        Self {
+            player_pos: (0, 1),
+            map_idx: 0,
+            find_start: false,
+            bliz,
+        }
+    }
+
+    fn tick(&self) -> Self {
+        Self {
+            player_pos: self.player_pos,
+            map_idx: self.map_idx + 1,
+            find_start: self.find_start,
+            bliz: self.bliz,
+        }
+    }
+
+    fn get(&self, row: i32, col: i32) -> u8 {
+        if row >= 0 && row < self.bliz.height {
+            self.bliz.get(self.map_idx, row, col)
+        } else {
+            Blizzard::WALL
+        }
+    }
 }
 
 impl Blizzard {
@@ -78,13 +154,12 @@ impl Blizzard {
         Self {
             width,
             height,
-            map: vec![0; (width * height) as usize],
-            player_pos: (0, 1),
+            maps: Vec::new(),
         }
     }
 
-    fn get(&self, row: i32, col: i32) -> u8 {
-        self.map[(row * self.width + col) as usize]
+    fn get(&self, idx: usize, row: i32, col: i32) -> u8 {
+        self.maps[idx % self.maps.len()][(row * self.width + col) as usize]
     }
 
     fn wrap(&self, from: (i32, i32), look: (i32, i32)) -> (i32, i32) {
@@ -97,65 +172,71 @@ impl Blizzard {
         }
     }
 
-    fn tick(&self) -> Self {
-        let mut map: Vec<u8> = Vec::new();
-
-        let mut idx = 0;
-        for row in 0..self.height {
-            for col in 0..self.width {
-                map.push(0);
-                let tile = self.get(row, col);
-                if row == 0 || row > self.height - 2 || col == 0 || col > self.width - 2 {
-                    map[idx] = tile;
-                } else {
-                    assert!(row > 0 && row < self.height - 1, "{row},{col}");
-                    assert!(col > 0 && col < self.width - 1, "{row},{col}");
-                    for l in Self::LOOK {
-                        let lrow = row + l.0 .0;
-                        let lcol = col + l.0 .1;
-                        let tile = self.get(lrow, lcol);
-                        if tile == Self::WALL {
-                            let (lrow, lcol) = self.wrap((row, col), l.0);
-                            map[idx] |= self.get(lrow, lcol) & l.1;
-                        } else {
-                            map[idx] |= tile & l.1;
+    fn generate_maps(&mut self) {
+        let mut map_idx = 0;
+        loop {
+            let mut map: Vec<u8> = Vec::new();
+            let mut idx = 0;
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    map.push(0);
+                    let tile = self.get(map_idx, row, col);
+                    if row == 0 || row > self.height - 2 || col == 0 || col > self.width - 2 {
+                        map[idx] = tile;
+                    } else {
+                        assert!(row > 0 && row < self.height - 1, "{row},{col}");
+                        assert!(col > 0 && col < self.width - 1, "{row},{col}");
+                        for l in Self::LOOK {
+                            let lrow = row + l.0 .0;
+                            let lcol = col + l.0 .1;
+                            let tile = self.get(map_idx, lrow, lcol);
+                            if tile == Self::WALL {
+                                let (lrow, lcol) = self.wrap((row, col), l.0);
+                                map[idx] |= self.get(map_idx, lrow, lcol) & l.1;
+                            } else {
+                                map[idx] |= tile & l.1;
+                            }
                         }
                     }
+                    idx += 1;
                 }
-                idx += 1;
             }
-        }
 
-        Self {
-            width: self.width,
-            height: self.height,
-            map,
-            player_pos: self.player_pos,
+            map_idx += 1;
+
+            if map == self.maps[0] {
+                break;
+            }
+            self.maps.push(map);
         }
     }
 
-    fn _draw(&self) {
+    fn _draw(&self, idx: usize, player_pos: (i32, i32)) {
         for row in 0..self.height {
             for col in 0..self.width {
-                print!(
-                    "{}",
-                    match self.get(row, col) {
-                        0 => '.',
-                        Self::UP => '^',
-                        Self::DOWN => 'v',
-                        Self::LEFT => '<',
-                        Self::RIGHT => '>',
-                        Self::WALL => '#',
-                        _ => 'X',
-                    }
-                );
+                if (row, col) == player_pos {
+                    print!("E");
+                } else {
+                    print!(
+                        "{}",
+                        match self.get(idx, row, col) {
+                            0 => '.',
+                            Self::UP => '^',
+                            Self::DOWN => 'v',
+                            Self::LEFT => '<',
+                            Self::RIGHT => '>',
+                            Self::WALL => '#',
+                            _ => 'X',
+                        }
+                    );
+                }
             }
             println!();
         }
     }
 }
 
-impl DijkstraSearch for Blizzard {
+impl<'a> DijkstraSearch for State<'a> {
     fn moves(&self) -> Vec<Self> {
         let mut moves = Vec::new();
 
@@ -167,8 +248,7 @@ impl DijkstraSearch for Blizzard {
         let move_right = (self.player_pos.0, self.player_pos.1 + 1);
 
         for new_pos in [stay_put, move_up, move_down, move_left, move_right] {
-            if new_pos.0 >= 0 && new_pos.0 < self.height && new_state.get(new_pos.0, new_pos.1) == 0
-            {
+            if new_state.get(new_pos.0, new_pos.1) == 0 {
                 let mut valid = new_state.clone();
                 valid.player_pos = new_pos;
                 moves.push(valid);
@@ -179,6 +259,10 @@ impl DijkstraSearch for Blizzard {
     }
 
     fn is_win_state(&self) -> bool {
-        self.player_pos == (self.height - 1, self.width - 2)
+        if self.find_start {
+            self.player_pos == (0, 1)
+        } else {
+            self.player_pos == (self.bliz.height - 1, self.bliz.width - 2)
+        }
     }
 }
