62ecee15bf343b109f368db0499ae41e72849ad1
Uncle Scientist <unclescientist@gmail.com>
2023-01-03 07:59:52 -0500

    2017 day 14 part 2

--- src/bin/run-aoc/aoc2017/aoc2017_14.rs
+++ src/bin/run-aoc/aoc2017/aoc2017_14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::Runner;
 
 use super::utils::knot_hash;
@@ -32,6 +34,38 @@ impl Runner for Aoc2017_14 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let mut grid = HashSet::new();
+        for row in 0..128 {
+            let mut hash = knot_hash(&format!("{}-{row}", self.key_string));
+            for col in 0..128 {
+                if hash & 1 == 1 {
+                    grid.insert((row, col));
+                }
+                hash >>= 1;
+            }
+        }
+
+        let mut count = 0;
+        let dir = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        for row in 0..128 {
+            for col in 0..128 {
+                if grid.contains(&(row, col)) {
+                    count += 1;
+
+                    let mut stack = vec![(row, col)];
+                    while let Some(pos) = stack.pop() {
+                        grid.remove(&pos);
+                        for d in &dir {
+                            if grid.contains(&(pos.0 + d.0, pos.1 + d.1)) {
+                                stack.push((pos.0 + d.0, pos.1 + d.1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        crate::output(count)
     }
 }
