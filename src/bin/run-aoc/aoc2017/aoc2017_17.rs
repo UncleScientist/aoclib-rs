5ee2532043a96046358c6db23eaf0afa9ac7d147
Uncle Scientist <unclescientist@gmail.com>
2023-01-05 08:15:11 -0500

    2017 day 17 part 1

---
+++ src/bin/run-aoc/aoc2017/aoc2017_17.rs
@@ -0,0 +1,68 @@
+use crate::Runner;
+
+#[derive(Default)]
+pub struct Aoc2017_17 {
+    // insert items here (or not, i'm not the boss of you)
+}
+
+impl Aoc2017_17 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2017_17 {
+    fn name(&self) -> (usize, usize) {
+        (2017, 17)
+    }
+
+    fn parse(&mut self) {}
+
+    fn part1(&mut self) -> Vec<String> {
+        let mut sl = Spinlock::new(349);
+
+        for i in 1..=2017 {
+            sl.one_step(i);
+        }
+
+        let pos = (sl.buffer.iter().position(|&x| x == 2017).unwrap() + 1) % sl.buffer.len();
+        crate::output(sl.buffer[pos])
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("unsolved")
+    }
+}
+
+#[derive(Debug)]
+struct Spinlock {
+    buffer: Vec<usize>,
+    step: usize,
+    current_pos: usize,
+}
+
+impl Spinlock {
+    fn new(step: usize) -> Self {
+        Self {
+            buffer: vec![0],
+            step,
+            current_pos: 0,
+        }
+    }
+
+    fn one_step(&mut self, num: usize) {
+        self.current_pos = (1 + (self.current_pos + self.step)) % self.buffer.len();
+        self.buffer.insert(self.current_pos, num);
+    }
+
+    fn _print(&self) {
+        for (i, c) in self.buffer.iter().enumerate() {
+            if i == self.current_pos {
+                print!("({c})");
+            } else {
+                print!(" {c} ");
+            }
+        }
+        println!();
+    }
+}
--- src/bin/run-aoc/aoc2017/mod.rs
+++ src/bin/run-aoc/aoc2017/mod.rs
@@ -18,6 +18,7 @@ mod aoc2017_13;
 mod aoc2017_14;
 mod aoc2017_15;
 mod aoc2017_16;
+mod aoc2017_17;
 
 use aoc2017_01::*;
 use aoc2017_02::*;
@@ -35,6 +36,7 @@ use aoc2017_13::*;
 use aoc2017_14::*;
 use aoc2017_15::*;
 use aoc2017_16::*;
+use aoc2017_17::*;
 
 pub fn run_2017(which: Selector) {
     let mut day01 = Aoc2017_01::new();
@@ -53,11 +55,12 @@ pub fn run_2017(which: Selector) {
     let mut day14 = Aoc2017_14::new();
     let mut day15 = Aoc2017_15::new();
     let mut day16 = Aoc2017_16::new();
+    let mut day17 = Aoc2017_17::new();
 
     let mut days: Vec<&mut dyn Runner> = vec![
         &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
         &mut day08, &mut day09, &mut day10, &mut day11, &mut day12, &mut day13, &mut day14,
-        &mut day15, &mut day16,
+        &mut day15, &mut day16, &mut day17,
     ];
 
     match which {
