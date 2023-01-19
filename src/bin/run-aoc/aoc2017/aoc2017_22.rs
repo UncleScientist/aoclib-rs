72d22b51de93cf4c341d31e063f472ff2ac0d696
Uncle Scientist <unclescientist@gmail.com>
2023-01-19 08:08:20 -0500

    2017 day 22 part 2

--- src/bin/run-aoc/aoc2017/aoc2017_22.rs
+++ src/bin/run-aoc/aoc2017/aoc2017_22.rs
@@ -1,10 +1,10 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use crate::Runner;
 
 #[derive(Default)]
 pub struct Aoc2017_22 {
-    infected_nodes: Vec<(i64, i64)>,
+    nodes: Vec<((i64, i64), State)>,
     width: i64,
     height: i64,
 }
@@ -28,29 +28,46 @@ impl Runner for Aoc2017_22 {
 
         for (row, line) in lines.iter().enumerate() {
             for (col, ch) in line.chars().enumerate() {
-                if ch == '#' {
-                    self.infected_nodes.push((row as i64, col as i64));
-                }
+                self.nodes.push((
+                    (row as i64, col as i64),
+                    if ch == '#' {
+                        State::Infected
+                    } else {
+                        State::Clean
+                    },
+                ));
             }
         }
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut grid = Grid::from_iter(self.infected_nodes.clone(), self.width, self.height);
+        let mut grid = Grid::from_iter(self.nodes.clone(), self.width, self.height);
         for _ in 0..10_000 {
-            grid.step();
+            grid.step(true);
         }
         crate::output(grid.infection_count)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let mut grid = Grid::from_iter(self.nodes.clone(), self.width, self.height);
+        for _ in 0..10000000 {
+            grid.step(false);
+        }
+        crate::output(grid.infection_count)
     }
+}
+
+#[derive(PartialEq, Eq, Debug, Clone)]
+enum State {
+    Clean,
+    Weakened,
+    Infected,
+    Flagged,
 }
 
 #[derive(Debug)]
 struct Grid {
-    nodes: HashSet<(i64, i64)>,
+    nodes: HashMap<(i64, i64), State>,
     pos: (i64, i64),
     dir: usize,
     infection_count: usize,
@@ -58,35 +75,66 @@ struct Grid {
 
 impl Grid {
     const DIR: [(i64, i64); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    const RIGHT: usize = 1;
+    const REVERSE: usize = 2;
+    const LEFT: usize = 3;
 
     fn _test_new() -> Self {
-        let testdata: [(i64, i64); 2] = [(0, 2), (1, 0)];
+        let testdata: [((i64, i64), State); 2] =
+            [((0, 2), State::Infected), ((1, 0), State::Infected)];
         Self {
-            nodes: HashSet::from_iter(testdata),
+            nodes: HashMap::from_iter(testdata),
             dir: 0,
             pos: (1, 1),
             infection_count: 0,
         }
     }
 
-    fn from_iter<I: IntoIterator<Item = (i64, i64)>>(iter: I, width: i64, height: i64) -> Self {
+    fn from_iter<I: IntoIterator<Item = ((i64, i64), State)>>(
+        iter: I,
+        width: i64,
+        height: i64,
+    ) -> Self {
         Self {
-            nodes: HashSet::from_iter(iter),
+            nodes: HashMap::from_iter(iter),
             pos: (width / 2, height / 2),
             dir: 0,
             infection_count: 0,
         }
     }
 
-    fn step(&mut self) {
-        if self.nodes.contains(&self.pos) {
-            self.dir = (self.dir + 1) % Self::DIR.len();
-            self.nodes.remove(&self.pos);
+    fn step(&mut self, part1: bool) {
+        let e = self.nodes.entry(self.pos).or_insert(State::Clean);
+        if part1 {
+            if *e == State::Infected {
+                self.dir = (self.dir + Self::RIGHT) % Self::DIR.len();
+                *e = State::Clean;
+            } else {
+                self.dir = (self.dir + Self::LEFT) % Self::DIR.len();
+                *e = State::Infected;
+                self.infection_count += 1;
+            }
         } else {
-            self.dir = (self.dir + Self::DIR.len() - 1) % Self::DIR.len();
-            self.nodes.insert(self.pos);
-            self.infection_count += 1;
+            match *e {
+                State::Clean => {
+                    self.dir = (self.dir + Self::LEFT) % Self::DIR.len();
+                    *e = State::Weakened;
+                }
+                State::Weakened => {
+                    self.infection_count += 1;
+                    *e = State::Infected;
+                }
+                State::Infected => {
+                    self.dir = (self.dir + Self::RIGHT) % Self::DIR.len();
+                    *e = State::Flagged;
+                }
+                State::Flagged => {
+                    self.dir = (self.dir + Self::REVERSE) % Self::DIR.len();
+                    *e = State::Clean;
+                }
+            }
         }
+
         self.pos.0 += Self::DIR[self.dir].0;
         self.pos.1 += Self::DIR[self.dir].1;
     }
