53f43ff8d2ba531f182186f486026db14b67c155
Uncle Scientist <unclescientist@gmail.com>
2023-11-01 08:33:28 -0400

    2018 day 3 part 2

--- src/bin/run-aoc/aoc2018/aoc2018_03.rs
+++ src/bin/run-aoc/aoc2018/aoc2018_03.rs
@@ -5,6 +5,7 @@ use crate::Runner;
 #[derive(Default)]
 pub struct Aoc2018_03 {
     claims: Vec<Claim>,
+    fabric: HashMap<(usize, usize), usize>,
 }
 
 impl Aoc2018_03 {
@@ -25,36 +26,47 @@ impl Runner for Aoc2018_03 {
             let (x, y) = xy.split_once(',').unwrap();
             let (w, h) = wh.split_once('x').unwrap();
 
-            let _id = id[1..].parse().unwrap();
+            let id = id[1..].parse().unwrap();
             let x = x.parse().unwrap();
             let y = y.parse().unwrap();
             let w = w.parse().unwrap();
             let h = h.parse().unwrap();
 
-            self.claims.push(Claim { _id, x, y, w, h });
+            self.claims.push(Claim { id, x, y, w, h });
         }
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut claim = HashMap::new();
         for c in &self.claims {
             for x in 0..c.w {
                 for y in 0..c.h {
-                    *claim.entry((c.x + x, c.y + y)).or_insert(0) += 1;
+                    *self.fabric.entry((c.x + x, c.y + y)).or_insert(0) += 1;
                 }
             }
         }
-        crate::output(claim.iter().filter(|(_pos, count)| **count >= 2).count())
+
+        crate::output(self.fabric.values().filter(|count| **count >= 2).count())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        'next_claim: for c in &self.claims {
+            for x in 0..c.w {
+                for y in 0..c.h {
+                    if self.fabric.get(&(c.x + x, c.y + y)) != Some(&1) {
+                        continue 'next_claim;
+                    }
+                }
+            }
+            return crate::output(c.id);
+        }
+
+        crate::output("** no answer found **")
     }
 }
 
 #[derive(Debug)]
 struct Claim {
-    _id: usize,
+    id: usize,
     x: usize,
     y: usize,
     w: usize,
