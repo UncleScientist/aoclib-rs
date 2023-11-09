33d438c72716ac05c4b2c98868c89b1bb591062b
Uncle Scientist <unclescientist@gmail.com>
2023-11-09 07:53:00 -0500

    2018 day 9 template

---
+++ src/bin/run-aoc/aoc2018/aoc2018_09.rs
@@ -0,0 +1,28 @@
+use crate::Runner;
+
+#[derive(Default)]
+pub struct Aoc2018_09 {
+    // insert items here (or not, i'm not the boss of you)
+}
+
+impl Aoc2018_09 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2018_09 {
+    fn name(&self) -> (usize, usize) {
+        (2018, 9)
+    }
+
+    fn parse(&mut self) {}
+
+    fn part1(&mut self) -> Vec<String> {
+        crate::output("unsolved")
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("unsolved")
+    }
+}
