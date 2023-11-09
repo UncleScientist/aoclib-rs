b8faf09793bc6dc9156473e4dc8210607e72cf20
Uncle Scientist <unclescientist@gmail.com>
2023-11-09 07:36:13 -0500

    2018 day 8 part 1

--- src/bin/run-aoc/aoc2018/aoc2018_08.rs
+++ src/bin/run-aoc/aoc2018/aoc2018_08.rs
@@ -1,8 +1,10 @@
+use std::slice::Iter;
+
 use crate::Runner;
 
 #[derive(Default)]
 pub struct Aoc2018_08 {
-    // insert items here (or not, i'm not the boss of you)
+    tree: Vec<u64>,
 }
 
 impl Aoc2018_08 {
@@ -16,13 +18,34 @@ impl Runner for Aoc2018_08 {
         (2018, 8)
     }
 
-    fn parse(&mut self) {}
+    fn parse(&mut self) {
+        self.tree = aoclib::numbers::<&str, u64>("input/2018-08.txt", ' ')
+            .first()
+            .unwrap()
+            .clone();
+    }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        crate::output(sum_metadata(&mut self.tree.iter()))
     }
 
     fn part2(&mut self) -> Vec<String> {
         crate::output("unsolved")
     }
+}
+
+fn sum_metadata(iter: &mut Iter<u64>) -> u64 {
+    let child_count = *iter.next().unwrap();
+    let metadata_count = *iter.next().unwrap();
+
+    let mut result = 0;
+    for _ in 0..child_count {
+        result += sum_metadata(iter);
+    }
+
+    for _ in 0..metadata_count {
+        result += *iter.next().unwrap();
+    }
+
+    result
 }
