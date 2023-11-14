ca150369463c5bb9cd2549ddf96509aced97fbaf
Uncle Scientist <unclescientist@gmail.com>
2023-11-14 08:38:52 -0500

    2018 day 12 part 2

--- src/bin/run-aoc/aoc2018/aoc2018_12.rs
+++ src/bin/run-aoc/aoc2018/aoc2018_12.rs
@@ -1,17 +1,49 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::Runner;
 
 #[derive(Default)]
 pub struct Aoc2018_12 {
     initial_state: VecDeque<bool>,
-    zero_point: usize,
+    zero_point: i64,
     notes: HashSet<usize>,
 }
 
 impl Aoc2018_12 {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    fn step(&self, mut current_zp: i64, state: &VecDeque<bool>) -> (i64, VecDeque<bool>) {
+        current_zp += 4;
+        let mut current_state = state.clone();
+
+        current_state.push_front(false);
+        current_state.push_front(false);
+        current_state.push_front(false);
+        current_state.push_front(false);
+        current_state.push_back(false);
+        current_state.push_back(false);
+        current_state.push_back(false);
+        current_state.push_back(false);
+
+        let mut next_state = VecDeque::new();
+        next_state.push_front(false);
+        next_state.push_front(false);
+        for group in current_state.make_contiguous().windows(5) {
+            next_state.push_back(self.notes.contains(&group.convert()));
+        }
+
+        while Some(&false) == next_state.front() {
+            next_state.pop_front();
+            current_zp -= 1;
+        }
+
+        while Some(&false) == next_state.back() {
+            next_state.pop_back();
+        }
+
+        (current_zp, next_state)
     }
 }
 
@@ -22,7 +54,6 @@ impl Runner for Aoc2018_12 {
 
     fn parse(&mut self) {
         let lines = aoclib::read_lines("input/2018-12.txt");
-        // let lines = aoclib::read_lines("test-input");
         let (_, istate) = lines[0].split_once(": ").unwrap();
 
         self.initial_state = istate
@@ -45,38 +76,10 @@ impl Runner for Aoc2018_12 {
 
     fn part1(&mut self) -> Vec<String> {
         let mut current_state = self.initial_state.clone();
-
         let mut current_zp = self.zero_point;
 
         for _ in 0..20 {
-            current_zp += 4;
-            current_state.push_front(false);
-            current_state.push_front(false);
-            current_state.push_front(false);
-            current_state.push_front(false);
-            current_state.push_back(false);
-            current_state.push_back(false);
-            current_state.push_back(false);
-            current_state.push_back(false);
-
-            let mut next_state = VecDeque::new();
-            next_state.push_front(false);
-            next_state.push_front(false);
-            for group in current_state.make_contiguous().windows(5) {
-                next_state.push_back(self.notes.contains(&group.convert()));
-            }
-
-            /*
-            let mut iter = next_state.iter();
-            while let Some(true) = iter.next() {
-                current_zp -= 1;
-            }
-            current_state.clear();
-            current_state.push_front(true);
-            current_state.extend(iter);
-            */
-            current_state = next_state;
-
+            (current_zp, current_state) = self.step(current_zp, &current_state);
             /*
             print!("{current_zp:4}: ");
             for val in current_state.iter() {
@@ -90,19 +93,29 @@ impl Runner for Aoc2018_12 {
             */
         }
 
-        let val = current_state
-            .iter()
-            .enumerate()
-            .filter(|(_, pot)| **pot)
-            .fold(0, |sum, (index, _)| {
-                sum + (index as i64 - current_zp as i64)
-            });
-
-        crate::output(val)
+        crate::output(current_state.calc_sum(current_zp))
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        let mut states = HashMap::new();
+        let mut count = 0;
+        let mut current_state = self.initial_state.clone();
+        let mut current_zp = self.zero_point;
+
+        // TODO: cleanup: don't assume the state moves 1 by 1 after looping
+        loop {
+            count += 1;
+            (current_zp, current_state) = self.step(current_zp, &current_state);
+            if let Some((pcount, pzp)) = states.get(&current_state) {
+                println!("prev = {pcount} @ {pzp}, curr = {count} @ {current_zp}");
+                break;
+            } else {
+                states.insert(current_state.clone(), (count, current_zp));
+            }
+        }
+
+        let remaining_generations = 50_000_000_000i64 - count;
+        crate::output(current_state.calc_sum(current_zp - remaining_generations))
     }
 }
 
@@ -129,5 +142,18 @@ impl Conversion for &[bool] {
             result += *bool as usize;
         }
         result
+    }
+}
+
+trait Summer {
+    fn calc_sum(&self, zp: i64) -> i64;
+}
+
+impl Summer for VecDeque<bool> {
+    fn calc_sum(&self, zp: i64) -> i64 {
+        self.iter()
+            .enumerate()
+            .filter(|(_, pot)| **pot)
+            .fold(0, |sum, (index, _)| sum + (index as i64 - zp))
     }
 }
