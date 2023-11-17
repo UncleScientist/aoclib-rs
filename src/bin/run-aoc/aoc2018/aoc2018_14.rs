8428d0f0a7dce25b5ede3fa696dc4b0e70119fc3
Uncle Scientist <unclescientist@gmail.com>
2023-11-17 08:45:11 -0500

    2018 day 14 part 2

--- src/bin/run-aoc/aoc2018/aoc2018_14.rs
+++ src/bin/run-aoc/aoc2018/aoc2018_14.rs
@@ -1,13 +1,48 @@
+use std::collections::VecDeque;
+
 use crate::Runner;
 
 #[derive(Default)]
 pub struct Aoc2018_14 {
     recipe_count: usize,
+    digits: Vec<u8>,
+    last_n: VecDeque<u8>,
+    recipes: Vec<u8>,
+    elf1: usize,
+    elf2: usize,
 }
 
 impl Aoc2018_14 {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    fn step(&mut self) {
+        let new_recipe = self.recipes[self.elf1] + self.recipes[self.elf2];
+
+        if new_recipe > 9 {
+            self.recipes.push(1);
+            if self.last_n.len() > self.digits.len() {
+                self.last_n.pop_front();
+            }
+            self.last_n.push_back(1);
+        }
+
+        if self.last_n.len() > self.digits.len() {
+            self.last_n.pop_front();
+        }
+        self.recipes.push(new_recipe % 10);
+        self.last_n.push_back(new_recipe % 10);
+
+        self.elf1 = (self.elf1 + self.recipes[self.elf1] as usize + 1) % self.recipes.len();
+        self.elf2 = (self.elf2 + self.recipes[self.elf2] as usize + 1) % self.recipes.len();
+    }
+
+    fn reset(&mut self) {
+        self.recipes = vec![3, 7];
+        self.elf1 = 0;
+        self.elf2 = 1;
+        self.last_n.clear();
     }
 }
 
@@ -18,31 +53,45 @@ impl Runner for Aoc2018_14 {
 
     fn parse(&mut self) {
         self.recipe_count = 939601;
+        self.reset();
+
+        let mut digits = Vec::new();
+        let mut num = self.recipe_count;
+        while num > 0 {
+            digits.push((num % 10) as u8);
+            num /= 10;
+        }
+        self.digits = digits.into_iter().rev().collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut recipes = vec![3, 7];
-        let mut elf1 = 0;
-        let mut elf2 = 1;
-
-        while recipes.len() < self.recipe_count + 10 {
-            let new_recipe = recipes[elf1] + recipes[elf2];
-            if new_recipe > 9 {
-                recipes.push(1);
-            }
-            recipes.push(new_recipe % 10);
-            elf1 = (elf1 + recipes[elf1] + 1) % recipes.len();
-            elf2 = (elf2 + recipes[elf2] + 1) % recipes.len();
+        while self.recipes.len() < self.recipe_count + 10 {
+            self.step();
         }
 
         let mut answer = String::new();
-        for c in &recipes[self.recipe_count..] {
-            answer.push((*c as u8 + b'0') as char);
+        for c in &self.recipes[self.recipe_count..] {
+            answer.push((*c + b'0') as char);
         }
         crate::output(answer)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        self.reset();
+        for _ in 0..self.digits.len() {
+            self.step();
+        }
+
+        'next: loop {
+            for (a, b) in self.digits.iter().zip(self.last_n.iter()) {
+                if a != b {
+                    self.step();
+                    continue 'next;
+                }
+            }
+
+            // total hack because I assume the puzzle input ends with a '1' digit
+            return crate::output(format!("{}", self.recipes.len() - self.digits.len() - 1));
+        }
     }
 }
