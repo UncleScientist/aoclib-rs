341b4c1f3d8ee71f908099562d7a6deffb1123e2
Uncle Scientist <unclescientist@gmail.com>
2023-12-05 07:41:19 -0500

    2023 day 5 part 1

--- crates/aoc2023/src/aoc2023_05.rs
+++ crates/aoc2023/src/aoc2023_05.rs
@@ -1,8 +1,11 @@
+use std::ops::Range;
+
 use aoclib::Runner;
 
 #[derive(Default)]
 pub struct Aoc2023_05 {
-    // insert items here (or not, i'm not the boss of you)
+    seeds: Vec<i64>,
+    mapping: Vec<Mapping>,
 }
 
 impl Aoc2023_05 {
@@ -16,13 +19,73 @@ impl Runner for Aoc2023_05 {
         (2023, 5)
     }
 
-    fn parse(&mut self) {}
+    fn parse(&mut self) {
+        let lines = aoclib::read_lines("input/2023-05.txt");
+        //let lines = aoclib::read_lines("test-input");
+        let seeds = lines[0].split_once(": ").unwrap().1;
+        self.seeds = seeds.split(' ').map(|seed| seed.parse().unwrap()).collect();
+
+        let mut curmap = Mapping::default();
+        for line in lines[2..].iter() {
+            if line.contains(":") {
+                self.mapping.push(curmap);
+                curmap = Mapping::default();
+                continue;
+            }
+            let nums: Vec<i64> = line.split(' ').map(|num| num.parse().unwrap()).collect();
+            curmap.add_mapping(nums[0], nums[1], nums[2]);
+        }
+        if !curmap.map.is_empty() {
+            self.mapping.push(curmap);
+        }
+    }
 
     fn part1(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let mut min = i64::MAX;
+
+        for seed in &self.seeds {
+            let mut cur = *seed;
+            for map in &self.mapping {
+                cur = map.apply_map(cur);
+            }
+            min = min.min(cur);
+        }
+        aoclib::output(min)
     }
 
     fn part2(&mut self) -> Vec<String> {
         aoclib::output("unsolved")
+    }
+}
+
+#[derive(Debug, Default)]
+struct SingleMap {
+    range: Range<i64>,
+    delta: i64,
+}
+
+#[derive(Debug, Default)]
+struct Mapping {
+    map: Vec<SingleMap>,
+}
+
+impl Mapping {
+    fn add_mapping(&mut self, dest: i64, src: i64, len: i64) {
+        self.map.push(SingleMap {
+            range: Range {
+                start: src,
+                end: src + len,
+            },
+            delta: dest - src,
+        });
+    }
+
+    fn apply_map(&self, val: i64) -> i64 {
+        for map in &self.map {
+            if map.range.contains(&val) {
+                return val + map.delta;
+            }
+        }
+        val
     }
 }
