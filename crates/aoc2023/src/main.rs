76af00c821df5c2c07ef6d61c53709976d0610fa
Uncle Scientist <unclescientist@gmail.com>
2023-12-05 07:21:20 -0500

    2023 day 5 template

--- crates/aoc2023/src/main.rs
+++ crates/aoc2023/src/main.rs
@@ -4,11 +4,13 @@ mod aoc2023_01;
 mod aoc2023_02;
 mod aoc2023_03;
 mod aoc2023_04;
+mod aoc2023_05;
 
 use aoc2023_01::*;
 use aoc2023_02::*;
 use aoc2023_03::*;
 use aoc2023_04::*;
+use aoc2023_05::*;
 
 fn main() {
     run_2023(Selector::Last);
@@ -19,8 +21,10 @@ fn run_2023(which: Selector) {
     let mut day02 = Aoc2023_02::new();
     let mut day03 = Aoc2023_03::new();
     let mut day04 = Aoc2023_04::new();
+    let mut day05 = Aoc2023_05::new();
 
-    let mut days: Vec<&mut dyn Runner> = vec![&mut day01, &mut day02, &mut day03, &mut day04];
+    let mut days: Vec<&mut dyn Runner> =
+        vec![&mut day01, &mut day02, &mut day03, &mut day04, &mut day05];
 
     match which {
         Selector::Last => {
