126826e1d4bc7b624be659ef41adc475e7e55f93
Uncle Scientist <unclescientist@gmail.com>
2023-12-06 07:42:18 -0500

    refactor part 1 in anticipation of part 2

--- crates/aoc2023/src/aoc2023_06.rs
+++ crates/aoc2023/src/aoc2023_06.rs
@@ -44,46 +44,44 @@ impl Runner for Aoc2023_06 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        // time t
-        // record r
-        // button_time 0..t
-        // movement_time (t - button_time)
-        // dist = button_time * (t - button_time)
-        // dist > r
-        //   button_time * (t - button_time) > r
-        // solve for button_time:  -button_time^2 + t * button_time - r > 0
-
-        let mut ans = 1;
-        for race in &self.races {
-            let a = -1f64;
-            let b = race.time as f64;
-            let c = (-race.record) as f64;
-
-            // print!("for race {race:?}: ");
-            let mut first = ((-b + (b * b - 4. * a * c).sqrt()) / (2. * a)).ceil();
-            let mut second = ((-b - (b * b - 4. * a * c).sqrt()) / (2. * a)).floor();
-
-            if (first * (b - first)) == race.record as f64 {
-                first += 1.;
-            }
-            if (second * (b - second)) == race.record as f64 {
-                second -= 1.;
-            }
-
-            // println!("{first}..{second}");
-
-            ans *= (second - first) as i64 + 1;
-        }
-        aoclib::output(ans)
+        aoclib::output(self.races.iter().fold(1, |acc, race| acc * race.wins()))
     }
 
     fn part2(&mut self) -> Vec<String> {
         aoclib::output("unsolved")
     }
 }
-
 #[derive(Debug)]
 struct RaceInfo {
     time: i64,
     record: i64,
+}
+
+impl RaceInfo {
+    // time t
+    // record r
+    // button_time -> 0..t
+    // movement_time -> (t - button_time)
+    // dist = button_time * (t - button_time)
+    // dist > r
+    //       button_time * (t - button_time) > r
+    // solve for button_time:  -button_time^2 + t * button_time - r > 0
+    fn wins(&self) -> i64 {
+        let a = -1f64;
+        let b = self.time as f64;
+        let c = (-self.record) as f64;
+
+        // print!("for race {self:?}: ");
+        let mut first = ((-b + (b * b - 4. * a * c).sqrt()) / (2. * a)).ceil();
+        let mut second = ((-b - (b * b - 4. * a * c).sqrt()) / (2. * a)).floor();
+
+        if (first * (b - first)) == self.record as f64 {
+            first += 1.;
+        }
+        if (second * (b - second)) == self.record as f64 {
+            second -= 1.;
+        }
+
+        (second - first) as i64 + 1
+    }
 }
