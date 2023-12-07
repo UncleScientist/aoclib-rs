fb2c07bac7f78f315335a054b5cc2e1845d3e1ef
Uncle Scientist <unclescientist@gmail.com>
2023-12-07 07:29:58 -0500

    2023 day 7 template

---
+++ crates/aoc2023/src/aoc2023_07.rs
@@ -0,0 +1,28 @@
+use aoclib::Runner;
+
+#[derive(Default)]
+pub struct Aoc2023_07 {
+    // insert items here (or not, i'm not the boss of you)
+}
+
+impl Aoc2023_07 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2023_07 {
+    fn name(&self) -> (usize, usize) {
+        (2023, 7)
+    }
+
+    fn parse(&mut self) {}
+
+    fn part1(&mut self) -> Vec<String> {
+        aoclib::output("unsolved")
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        aoclib::output("unsolved")
+    }
+}
