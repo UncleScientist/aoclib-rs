d08bfa816b74ed7b58e7e9433015b4df97cc94f3
Uncle Scientist <unclescientist@gmail.com>
2023-12-11 07:45:16 -0500

    2023 day 11 part 2

--- crates/aoc2023/src/aoc2023_11.rs
+++ crates/aoc2023/src/aoc2023_11.rs
@@ -13,6 +13,29 @@ impl Aoc2023_11 {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn calculate_distances(&self, scale: i64) -> i64 {
+        let galaxies = self.galaxies.iter().collect::<Vec<_>>();
+        let scale = scale - 1;
+
+        let mut total_distance = 0;
+        for (i, first) in galaxies.iter().enumerate() {
+            let (irow, icol) = (
+                first.0 + self.row_offsets[first.0 as usize] * scale,
+                first.1 + self.col_offsets[first.1 as usize] * scale,
+            );
+
+            for second in galaxies.iter().skip(i) {
+                let (jrow, jcol) = (
+                    second.0 + self.row_offsets[second.0 as usize] * scale,
+                    second.1 + self.col_offsets[second.1 as usize] * scale,
+                );
+                total_distance += (irow - jrow).abs() + (icol - jcol).abs();
+            }
+        }
+
+        total_distance
+    }
 }
 
 impl Runner for Aoc2023_11 {
@@ -52,28 +75,10 @@ impl Runner for Aoc2023_11 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let galaxies = self.galaxies.iter().collect::<Vec<_>>();
-
-        let mut total_distance = 0;
-        for (i, first) in galaxies.iter().enumerate() {
-            let (irow, icol) = (
-                first.0 + self.row_offsets[first.0 as usize],
-                first.1 + self.col_offsets[first.1 as usize],
-            );
-
-            for second in galaxies.iter().skip(i) {
-                let (jrow, jcol) = (
-                    second.0 + self.row_offsets[second.0 as usize],
-                    second.1 + self.col_offsets[second.1 as usize],
-                );
-                total_distance += (irow - jrow).abs() + (icol - jcol).abs();
-            }
-        }
-
-        aoclib::output(total_distance)
+        aoclib::output(self.calculate_distances(2))
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        aoclib::output(self.calculate_distances(1_000_000))
     }
 }
