d6adfe51a35349143f7fe14d0126b11cf421f4a7
Uncle Scientist <unclescientist@gmail.com>
2023-12-15 08:00:35 -0500

    2023 day 15 part 2

--- crates/aoc2023/src/aoc2023_15.rs
+++ crates/aoc2023/src/aoc2023_15.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, str::FromStr};
+
 use aoclib::Runner;
 
 #[derive(Default)]
@@ -26,7 +28,40 @@ impl Runner for Aoc2023_15 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        // let mut boxes = vec![Vec::<Vec<Lens>>::new(); 256];
+        let mut boxes: [Vec<Lens>; 256] = std::array::from_fn(|_| Vec::new());
+
+        for step in &self.steps {
+            let command: Step = step.parse().unwrap();
+            match command {
+                Step::Replace(label, focal_length) => {
+                    let index = label.aoc_hash();
+                    if let Some(lens) = boxes[index].iter_mut().find(|lens| lens.label == label) {
+                        lens.focal_length = focal_length;
+                    } else {
+                        boxes[index].push(Lens::new(label, focal_length));
+                    }
+                }
+                Step::Remove(label) => {
+                    let index = label.aoc_hash();
+                    boxes[index].retain(|lens| lens.label != label);
+                }
+            }
+        }
+
+        aoclib::output(
+            boxes
+                .iter()
+                .enumerate()
+                .map(|(box_number, lens_list)| {
+                    lens_list
+                        .iter()
+                        .enumerate()
+                        .map(|(slot, lens)| (box_number + 1) * (slot + 1) * lens.focal_length)
+                        .sum::<usize>()
+                })
+                .sum::<usize>(),
+        )
     }
 }
 
@@ -34,9 +69,47 @@ trait AocHash {
     fn aoc_hash(&self) -> usize;
 }
 
-impl AocHash for String {
+impl<T: AsRef<str>> AocHash for T {
     fn aoc_hash(&self) -> usize {
-        self.chars()
+        self.as_ref()
+            .chars()
             .fold(0, |acc, ch| ((ch as usize + acc) * 17) % 256)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Lens {
+    label: String,
+    focal_length: usize,
+}
+
+impl Lens {
+    fn new<D: Display>(label: D, focal_length: usize) -> Self {
+        Self {
+            label: label.to_string(),
+            focal_length,
+        }
+    }
+}
+
+enum Step {
+    Replace(String, usize),
+    Remove(String),
+}
+
+impl FromStr for Step {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some((label, focal_length)) = s.split_once('=') {
+            Ok(Self::Replace(
+                label.to_string(),
+                focal_length.parse().unwrap(),
+            ))
+        } else if let Some(label) = s.strip_suffix('-') {
+            Ok(Self::Remove(label.to_string()))
+        } else {
+            Err(format!("Cannot parse '{s}'"))
+        }
     }
 }
