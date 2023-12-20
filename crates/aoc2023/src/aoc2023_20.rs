a0ebb3b579b9f37f026a6edac871c488855c3660
Uncle Scientist <unclescientist@gmail.com>
2023-12-20 08:30:54 -0500

    2023 day 20 part 2

--- crates/aoc2023/src/aoc2023_20.rs
+++ crates/aoc2023/src/aoc2023_20.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, VecDeque};
 
-use aoclib::Runner;
+use aoclib::{Mathemagic, Runner};
 
 #[derive(Default)]
 pub struct Aoc2023_20 {
@@ -12,12 +12,17 @@ impl Aoc2023_20 {
         Self::default()
     }
 
-    fn pulse(&mut self) -> (i64, i64) {
+    fn pulse(&mut self, keys: &[String]) -> (u64, u64) {
         let mut queue =
             VecDeque::from_iter([("button".to_string(), "broadcaster".to_string(), false)]);
         let mut pcount = [0, 0];
+        let mut this_cycle = 0;
 
         while let Some((sender, dest, pulse)) = queue.pop_front() {
+            if dest == "rx" && !pulse {
+                return (0, 0);
+            }
+
             pcount[pulse as usize] += 1;
             if let Some(Module { modtype, outputs }) = self.modules.get_mut(&dest) {
                 match modtype {
@@ -32,6 +37,11 @@ impl Aoc2023_20 {
                     ModuleType::Conjunction(ss) => {
                         ss.insert(sender.clone(), pulse);
                         let out = !ss.values().all(|val| *val);
+                        if !keys.is_empty() && out {
+                            if let Some(p) = keys.iter().position(|k| k == &dest) {
+                                this_cycle = p + 1;
+                            }
+                        }
                         for o in outputs {
                             queue.push_back((dest.clone(), o.clone(), out));
                         }
@@ -45,7 +55,11 @@ impl Aoc2023_20 {
             }
         }
 
-        (pcount[0], pcount[1])
+        if this_cycle != 0 {
+            (0, this_cycle as u64 - 1)
+        } else {
+            (pcount[0], pcount[1])
+        }
     }
 }
 
@@ -109,7 +123,7 @@ impl Runner for Aoc2023_20 {
     fn part1(&mut self) -> Vec<String> {
         let mut counts = [0, 0];
         for _ in 0..1000 {
-            let (a, b) = self.pulse();
+            let (a, b) = self.pulse(&[]);
             counts[0] += a;
             counts[1] += b;
         }
@@ -117,7 +131,57 @@ impl Runner for Aoc2023_20 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        // let cycle = [3847u64, 3851, 4003, 4027];
+        // println!("lcm of {cycle:?} = {}", cycle.lcm().unwrap());
+
+        // reset all flip flops and conjunctions
+        for Module { modtype, .. } in self.modules.values_mut() {
+            match modtype {
+                ModuleType::FlipFlop(s) => {
+                    *s = false;
+                }
+                ModuleType::Conjunction(ss) => {
+                    for s in ss.values_mut() {
+                        *s = false;
+                    }
+                }
+                ModuleType::Broadcaster => {}
+            }
+        }
+
+        let mut source = None;
+        for (name, Module { outputs, .. }) in self.modules.iter() {
+            if outputs.contains(&"rx".to_string()) {
+                source = Some(name);
+                break;
+            }
+        }
+        let Some(source) = source else {
+            panic!("could not find predecessor to rx");
+        };
+
+        let mut prepre = Vec::new();
+        for (name, Module { outputs, .. }) in self.modules.iter() {
+            if outputs.contains(source) {
+                prepre.push(name.clone());
+            }
+        }
+
+        let mut loop_count = 0;
+        let mut found = 0;
+        let mut cycles = vec![0u64; prepre.len()];
+        loop {
+            loop_count += 1;
+            if let (0, c) = self.pulse(&prepre) {
+                cycles[c as usize] = loop_count;
+                found += 1;
+                if found == prepre.len() {
+                    break;
+                }
+            }
+        }
+
+        aoclib::output(cycles.lcm().unwrap())
     }
 }
 
