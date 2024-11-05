ea4e0a8a318124e1b8cc5242ce8d63347a7c40f9
Uncle Scientist <unclescientist@gmail.com>
2024-11-05 08:22:05 -0500

    upgraded intcode vm to handle resets

--- crates/aoc2019/src/intcode.rs
+++ crates/aoc2019/src/intcode.rs
@@ -1,23 +1,27 @@
 #[derive(Debug, Default)]
 pub struct Intcode {
+    reset: Vec<i64>,
     memory: Vec<i64>,
     ip: usize,
     running: bool,
 }
 
 impl Intcode {
-    pub fn new_from_string(s: &str) -> Self {
+    pub fn new(s: &str) -> Self {
+        let reset: Vec<i64> = s
+            .split(',')
+            .map(|num| num.parse::<i64>().unwrap())
+            .collect();
         Intcode {
-            memory: s
-                .split(',')
-                .map(|num| num.parse::<i64>().unwrap())
-                .collect(),
-            ip: 0,
-            running: true,
+            memory: reset.clone(),
+            reset,
+            ..Self::default()
         }
     }
 
     pub fn run(&mut self) {
+        self.running = true;
+
         while self.running {
             self.step();
             self.ip += 4;
@@ -42,7 +46,7 @@ impl Intcode {
             99 => {
                 self.running = false;
             }
-            _ => panic!("instruction {inst} unimplemented"),
+            _ => panic!("instruction {inst} at ip {} unimplemented", self.ip),
         }
     }
 
@@ -53,6 +57,11 @@ impl Intcode {
     pub(crate) fn peek(&self, address: i64) -> i64 {
         self.memory[address as usize]
     }
+
+    pub(crate) fn reset(&mut self) {
+        self.memory = self.reset.clone();
+        self.ip = 0;
+    }
 }
 
 #[cfg(test)]
@@ -61,28 +70,28 @@ mod tests {
 
     #[test]
     fn test_halt() {
-        let mut computer = Intcode::new_from_string("99");
+        let mut computer = Intcode::new("99");
         computer.run();
         assert!(!computer.running);
     }
 
     #[test]
     fn test_add() {
-        let mut computer = Intcode::new_from_string("1,0,2,0,99");
+        let mut computer = Intcode::new("1,0,2,0,99");
         computer.run();
         assert_eq!(vec![3, 0, 2, 0, 99], computer.memory);
     }
 
     #[test]
     fn test_multiplication() {
-        let mut computer = Intcode::new_from_string("2,4,2,0,99");
+        let mut computer = Intcode::new("2,4,2,0,99");
         computer.run();
         assert_eq!(vec![2 * 99, 4, 2, 0, 99], computer.memory);
     }
 
     #[test]
     fn test_peek() {
-        let computer = Intcode::new_from_string("2,4,2,0,99");
+        let computer = Intcode::new("2,4,2,0,99");
         assert_eq!(2, computer.peek(0));
         assert_eq!(4, computer.peek(1));
         assert_eq!(2, computer.peek(2));
@@ -92,7 +101,7 @@ mod tests {
 
     #[test]
     fn test_poke() {
-        let mut computer = Intcode::new_from_string("2,4,2,0,99");
+        let mut computer = Intcode::new("2,4,2,0,99");
         computer.poke(3, 1234);
         assert_eq!(2, computer.peek(0));
         assert_eq!(4, computer.peek(1));
@@ -114,9 +123,21 @@ mod tests {
             ("1,1,1,4,99,5,6,0,99", vec![30, 1, 1, 4, 2, 5, 6, 0, 99]),
         ];
         for entry in matrix {
-            let mut computer = Intcode::new_from_string(entry.0);
+            let mut computer = Intcode::new(entry.0);
             computer.run();
             assert_eq!(entry.1, computer.memory);
         }
+    }
+
+    #[test]
+    fn can_reset_program() {
+        let mut computer = Intcode::new("1,9,10,3,2,3,11,0,99,30,40,50");
+        computer.run();
+        computer.reset();
+        assert_eq!(
+            vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+            computer.memory,
+        );
+        assert_eq!(0, computer.ip);
     }
 }
