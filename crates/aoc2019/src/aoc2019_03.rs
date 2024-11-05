278e5641353b87721cedc45c528216094f8ca7ae
Uncle Scientist <unclescientist@gmail.com>
2024-11-05 09:00:23 -0500

    2019 day 3 part 2

--- crates/aoc2019/src/aoc2019_03.rs
+++ crates/aoc2019/src/aoc2019_03.rs
@@ -40,13 +40,19 @@ impl Runner for Aoc2019_03 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        aoclib::output(
+            self.intersections
+                .iter()
+                .map(|int| self.wire1.get(int).unwrap() + self.wire2.get(int).unwrap())
+                .min()
+                .unwrap(),
+        )
     }
 }
 
 fn parse_line(lines: &str, wire: &mut HashMap<(i64, i64), usize>) {
     let (mut x, mut y) = (0, 0);
-    let mut steps = 0;
+    let mut steps = 1;
 
     for inst in lines.split(',') {
         let dir = &inst[0..1];
@@ -65,5 +71,45 @@ fn parse_line(lines: &str, wire: &mut HashMap<(i64, i64), usize>) {
             wire.entry((x, y)).or_insert(steps);
             steps += 1;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn run_wires(wire1: &str, wire2: &str) -> usize {
+        let mut aoc = Aoc2019_03::default();
+        parse_line(wire1, &mut aoc.wire1);
+        parse_line(wire2, &mut aoc.wire2);
+        aoc.part1();
+        aoc.part2()[0].parse().unwrap()
+    }
+
+    #[test]
+    fn test1() {
+        assert_eq!(30, run_wires("R8,U5,L5,D3", "U7,R6,D4,L4"));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            610,
+            run_wires(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+            )
+        );
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(
+            410,
+            run_wires(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            )
+        );
     }
 }
