4849de0a503ed0d92b54660a7d4cbf2735c12590
Uncle Scientist <unclescientist@gmail.com>
2024-11-07 07:30:21 -0500

    2019 day 4 part 2

--- crates/aoc2019/src/aoc2019_04.rs
+++ crates/aoc2019/src/aoc2019_04.rs
@@ -31,7 +31,10 @@ impl Runner for Aoc2019_04 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let count = (self.low..=self.high)
+            .filter_map(validate_single_double)
+            .count();
+        aoclib::output(count)
     }
 }
 
@@ -45,5 +48,54 @@ fn validate_num(num: usize) -> Option<()> {
         Some(())
     } else {
         None
+    }
+}
+
+fn validate_single_double(num: usize) -> Option<()> {
+    let digits = format!("{num}").chars().collect::<Vec<_>>();
+
+    if !digits.windows(2).all(|w| w[0] <= w[1]) {
+        return None;
+    }
+
+    let mut iter = digits.iter();
+    let mut prev = iter.next().unwrap();
+    let mut count = 1;
+    while let Some(digit) = iter.next() {
+        if prev == digit {
+            count += 1;
+        } else {
+            if count == 2 {
+                return Some(());
+            }
+            count = 1;
+        }
+        prev = digit;
+    }
+
+    if count == 2 {
+        Some(())
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert!(validate_single_double(112233).is_some());
+    }
+
+    #[test]
+    fn test2() {
+        assert!(validate_single_double(123444).is_none());
+    }
+
+    #[test]
+    fn test3() {
+        assert!(validate_single_double(111122).is_some());
     }
 }
