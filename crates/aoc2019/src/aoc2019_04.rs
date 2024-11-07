aa371b391291d14e0305a9e37a7df7a9467cc88b
Uncle Scientist <unclescientist@gmail.com>
2024-11-07 07:22:18 -0500

    2019 day 4 part 1

--- crates/aoc2019/src/aoc2019_04.rs
+++ crates/aoc2019/src/aoc2019_04.rs
@@ -1,8 +1,11 @@
 use aoclib::Runner;
 
+const RANGE: &str = "109165-576723";
+
 #[derive(Default)]
 pub struct Aoc2019_04 {
-    // insert items here (or not, i'm not the boss of you)
+    low: usize,
+    high: usize,
 }
 
 impl Aoc2019_04 {
@@ -16,13 +19,31 @@ impl Runner for Aoc2019_04 {
         (2019, 4)
     }
 
-    fn parse(&mut self) {}
+    fn parse(&mut self) {
+        let (low, high) = RANGE.split_once('-').unwrap();
+        self.low = low.parse::<usize>().unwrap();
+        self.high = high.parse::<usize>().unwrap();
+    }
 
     fn part1(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let count = (self.low..=self.high).filter_map(validate_num).count();
+        aoclib::output(count)
     }
 
     fn part2(&mut self) -> Vec<String> {
         aoclib::output("unsolved")
+    }
+}
+
+fn validate_num(num: usize) -> Option<()> {
+    let digits = format!("{num}").chars().collect::<Vec<_>>();
+
+    let increasing = digits.windows(2).all(|w| w[0] <= w[1]);
+    let double = digits.windows(2).any(|w| w[0] == w[1]);
+
+    if increasing && double {
+        Some(())
+    } else {
+        None
     }
 }
