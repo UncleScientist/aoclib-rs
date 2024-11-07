881bbd21eb67efadd3f1c20de523996cc46ceb25
Uncle Scientist <unclescientist@gmail.com>
2024-11-07 07:07:34 -0500

    setup for 2019 day 4

--- crates/aoc2019/src/main.rs
+++ crates/aoc2019/src/main.rs
@@ -5,10 +5,12 @@ mod intcode;
 mod aoc2019_01;
 mod aoc2019_02;
 mod aoc2019_03;
+mod aoc2019_04;
 
 use aoc2019_01::*;
 use aoc2019_02::*;
 use aoc2019_03::*;
+use aoc2019_04::*;
 
 pub fn main() {
     run_2019(Selector::Last);
@@ -18,8 +20,9 @@ pub fn run_2019(which: Selector) {
     let mut day01 = Aoc2019_01::new();
     let mut day02 = Aoc2019_02::new();
     let mut day03 = Aoc2019_03::new();
+    let mut day04 = Aoc2019_04::new();
 
-    let mut days: Vec<&mut dyn Runner> = vec![&mut day01, &mut day02, &mut day03];
+    let mut days: Vec<&mut dyn Runner> = vec![&mut day01, &mut day02, &mut day03, &mut day04];
 
     match which {
         Selector::Last => {
