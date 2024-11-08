ad612e359946d6fd9e9f48b995cb35dd0fcc9e58
Uncle Scientist <unclescientist@gmail.com>
2024-11-08 07:51:32 -0500

    prep for 2019 day 5

---
+++ crates/aoc2019/src/aoc2019_05.rs
@@ -0,0 +1,28 @@
+use aoclib::Runner;
+
+#[derive(Default)]
+pub struct Aoc2019_05 {
+    // insert items here (or not, i'm not the boss of you)
+}
+
+impl Aoc2019_05 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2019_05 {
+    fn name(&self) -> (usize, usize) {
+        (2019, 5)
+    }
+
+    fn parse(&mut self) {}
+
+    fn part1(&mut self) -> Vec<String> {
+        aoclib::output("unsolved")
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        aoclib::output("unsolved")
+    }
+}
--- crates/aoc2019/src/main.rs
+++ crates/aoc2019/src/main.rs
@@ -6,11 +6,13 @@ mod aoc2019_01;
 mod aoc2019_02;
 mod aoc2019_03;
 mod aoc2019_04;
+mod aoc2019_05;
 
 use aoc2019_01::*;
 use aoc2019_02::*;
 use aoc2019_03::*;
 use aoc2019_04::*;
+use aoc2019_05::*;
 
 pub fn main() {
     run_2019(Selector::Last);
@@ -21,8 +23,10 @@ pub fn run_2019(which: Selector) {
     let mut day02 = Aoc2019_02::new();
     let mut day03 = Aoc2019_03::new();
     let mut day04 = Aoc2019_04::new();
+    let mut day05 = Aoc2019_05::new();
 
-    let mut days: Vec<&mut dyn Runner> = vec![&mut day01, &mut day02, &mut day03, &mut day04];
+    let mut days: Vec<&mut dyn Runner> =
+        vec![&mut day01, &mut day02, &mut day03, &mut day04, &mut day05];
 
     match which {
         Selector::Last => {
