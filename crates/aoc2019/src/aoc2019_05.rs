6c98d115fffa88bfe268b38cf8cb1a5d3260af53
Uncle Scientist <unclescientist@gmail.com>
2024-11-08 08:04:49 -0500

    2019 day 5 part 1

--- crates/aoc2019/src/aoc2019_05.rs
+++ crates/aoc2019/src/aoc2019_05.rs
@@ -1,8 +1,10 @@
 use aoclib::Runner;
 
+use crate::intcode::Intcode;
+
 #[derive(Default)]
 pub struct Aoc2019_05 {
-    // insert items here (or not, i'm not the boss of you)
+    computer: Intcode,
 }
 
 impl Aoc2019_05 {
@@ -16,10 +18,20 @@ impl Runner for Aoc2019_05 {
         (2019, 5)
     }
 
-    fn parse(&mut self) {}
+    fn parse(&mut self) {
+        let lines = aoclib::read_lines("input/2019-05.txt");
+        self.computer = Intcode::new(&lines[0]);
+    }
 
     fn part1(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        self.computer.push(1);
+        while let Some(val) = self.computer.run_until_output() {
+            if val != 0 {
+                assert!(self.computer.run_until_output().is_none());
+                return aoclib::output(val);
+            }
+        }
+        panic!("no solution found");
     }
 
     fn part2(&mut self) -> Vec<String> {
