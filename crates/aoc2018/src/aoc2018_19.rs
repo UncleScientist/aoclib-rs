013b0e448e39b00bc94d99a1dd0a4fa377c9ee66
Uncle Scientist <unclescientist@gmail.com>
2024-11-11 11:08:57 -0500

    2018 day 19 part 2

--- crates/aoc2018/src/aoc2018_19.rs
+++ crates/aoc2018/src/aoc2018_19.rs
@@ -5,7 +5,6 @@ use aoclib::Runner;
 #[derive(Default)]
 pub struct Aoc2018_19 {
     computer: Machine,
-    program: Vec<(Opcode, i64, i64, i64)>,
 }
 
 impl Aoc2018_19 {
@@ -24,38 +23,27 @@ impl Runner for Aoc2018_19 {
         let mut iter = lines.iter();
         let (_, ip_reg_num) = iter.next().unwrap().split_once(' ').unwrap();
 
+        self.computer = Machine::new(ip_reg_num.parse().unwrap());
+
         for line in iter {
             let instruction: Vec<&str> = line.split(' ').collect();
-            self.program.push((
+            self.computer.program.push((
                 instruction[0].parse().unwrap(),
                 instruction[1].parse().unwrap(),
                 instruction[2].parse().unwrap(),
                 instruction[3].parse().unwrap(),
             ));
         }
-        self.computer = Machine::new(ip_reg_num.parse().unwrap());
     }
 
     fn part1(&mut self) -> Vec<String> {
-        loop {
-            let ip = self.computer.regs[self.computer.ip] as usize;
-            if ip >= self.program.len() {
-                break;
-            }
-            // print!("{ip}: ");
-            self.computer.step(
-                self.program[ip].0,
-                self.program[ip].1,
-                self.program[ip].2,
-                self.program[ip].3,
-            );
-            self.computer.regs[self.computer.ip] += 1;
-        }
-        aoclib::output(self.computer.regs[0])
+        aoclib::output(self.computer.run())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        self.computer.reset();
+        self.computer.regs[0] = 1;
+        aoclib::output(self.computer.run())
     }
 }
 
@@ -107,13 +95,22 @@ impl FromStr for Opcode {
 
 #[derive(Default)]
 struct Machine {
+    program: Vec<(Opcode, i64, i64, i64)>,
     regs: [i64; 6],
     ip: usize, // which register is the ip
 }
 
 impl Machine {
     fn new(ip: usize) -> Self {
-        Self { regs: [0; 6], ip }
+        Self {
+            program: Vec::new(),
+            regs: [0; 6],
+            ip,
+        }
+    }
+
+    fn reset(&mut self) {
+        self.regs = [0; 6];
     }
 
     fn store(&mut self, reg: i64, val: i64) {
@@ -144,5 +141,30 @@ impl Machine {
             Opcode::EqRR => self.store(c, (reg_a == reg_b) as i64),
         }
         // println!("{:?}", self.regs);
+    }
+
+    fn run(&mut self) -> i64 {
+        loop {
+            let ip = self.regs[self.ip] as usize;
+            if ip == 1 {
+                // looking at the code, we can see we're just summing up
+                // the factors of the number that ends up in register 4
+                return (1..=self.regs[4])
+                    .filter(|div| self.regs[4] % div == 0)
+                    .sum();
+            }
+            let reg0 = self.regs[0];
+            if ip >= self.program.len() {
+                return self.regs[0];
+            }
+            // print!("{ip}: ");
+            self.step(
+                self.program[ip].0,
+                self.program[ip].1,
+                self.program[ip].2,
+                self.program[ip].3,
+            );
+            self.regs[self.ip] += 1;
+        }
     }
 }
