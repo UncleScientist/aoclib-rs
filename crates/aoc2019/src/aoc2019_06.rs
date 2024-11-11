a187c65f80b9e909a8a37b6a17f4843f6f9a3d71
Uncle Scientist <unclescientist@gmail.com>
2024-11-11 11:40:12 -0500

    2019 day 6 part 2

--- crates/aoc2019/src/aoc2019_06.rs
+++ crates/aoc2019/src/aoc2019_06.rs
@@ -28,7 +28,7 @@ impl Runner for Aoc2019_06 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        aoclib::output(self.orbital_transfers().unwrap())
     }
 }
 
@@ -51,6 +51,30 @@ impl Aoc2019_06 {
 
         total
     }
+
+    fn orbital_transfers(&self) -> Option<usize> {
+        let mut you2com = HashMap::<String, usize>::new();
+
+        let mut object = self.orbit.get("YOU").unwrap();
+        let mut steps = 1;
+        while let Some(next) = self.orbit.get(object) {
+            you2com.insert(next.to_string(), steps);
+            steps += 1;
+            object = next;
+        }
+
+        let mut object = self.orbit.get("SAN").unwrap();
+        let mut steps = 1;
+        while let Some(next) = self.orbit.get(object) {
+            if let Some(yousteps) = you2com.get(next) {
+                return Some(steps + yousteps);
+            }
+            steps += 1;
+            object = next;
+        }
+
+        None
+    }
 }
 
 #[cfg(test)]
@@ -65,5 +89,15 @@ mod tests {
         ]);
 
         assert_eq!(42, aoc.count_orbits());
+    }
+
+    #[test]
+    fn example2() {
+        let mut aoc = Aoc2019_06::default();
+        aoc.init(&[
+            "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU",
+            "I)SAN",
+        ]);
+        assert_eq!(Some(4), aoc.orbital_transfers());
     }
 }
