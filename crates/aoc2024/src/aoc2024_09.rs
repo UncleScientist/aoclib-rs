b30b51acba45c586636f7fca2c62557542764a6a
Uncle Scientist <unclescientist@gmail.com>
2024-12-09 08:42:01 -0500

    2024 day 9 part 2

--- crates/aoc2024/src/aoc2024_09.rs
+++ crates/aoc2024/src/aoc2024_09.rs
@@ -2,7 +2,7 @@ use aoclib::Runner;
 
 #[derive(Default)]
 pub struct Aoc2024_09 {
-    disk: Vec<char>,
+    disk: Vec<usize>,
 }
 
 impl Aoc2024_09 {
@@ -17,7 +17,11 @@ impl Runner for Aoc2024_09 {
     }
 
     fn parse(&mut self) {
-        self.disk = aoclib::read_single_line("input/2024-09.txt");
+        let line = aoclib::read_single_line("input/2024-09.txt");
+        self.disk = line
+            .iter()
+            .map(|ch| ((*ch as u8) - b'0') as usize)
+            .collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
@@ -25,12 +29,10 @@ impl Runner for Aoc2024_09 {
         let mut disk = Vec::new();
         let mut iter = self.disk.iter();
         while let Some(len) = iter.next() {
-            let len = (*len as u8) - b'0';
-            disk.extend(vec![fileno; len as usize]);
+            disk.extend(vec![fileno; *len]);
             fileno += 1;
             if let Some(gap) = iter.next() {
-                let gap = (*gap as u8) - b'0';
-                disk.extend(vec![-1; gap as usize]);
+                disk.extend(vec![-1; *gap]);
             }
         }
 
@@ -57,6 +59,79 @@ impl Runner for Aoc2024_09 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let mut fileno = 0;
+        let mut iter = self.disk.iter();
+
+        let mut file_list = Vec::new();
+        let mut gap_list = Vec::new();
+
+        let mut pos = 0;
+        while let Some(len) = iter.next() {
+            file_list.push((pos, len, fileno));
+            fileno += 1;
+            pos += len;
+            if let Some(gap) = iter.next() {
+                gap_list.push((pos, *gap));
+                pos += gap;
+            }
+        }
+
+        let mut updated_files = Vec::new();
+
+        while let Some(mut file) = file_list.pop() {
+            let mut moved = false;
+            for i in 0..gap_list.len() {
+                if gap_list[i].0 >= file.0 {
+                    updated_files.push(file);
+                    moved = true;
+                    break;
+                }
+
+                if gap_list[i].1 >= *file.1 {
+                    file.0 = gap_list[i].0;
+                    updated_files.push(file);
+                    moved = true;
+
+                    if gap_list[i].1 > *file.1 {
+                        gap_list[i].0 += *file.1;
+                        gap_list[i].1 -= *file.1;
+                    } else {
+                        gap_list.remove(i);
+                    }
+                    break;
+                }
+            }
+            if !moved {
+                updated_files.push(file);
+            }
+        }
+        updated_files.sort_by_key(|file| file.0);
+
+        let total = updated_files
+            .iter()
+            .map(|(pos, len, fileno)| {
+                (*pos..*pos + **len)
+                    .map(|index| index * fileno)
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+        aoclib::output(total)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_2() {
+        let mut aoc = Aoc2024_09 {
+            disk: "2333133121414131402"
+                .chars()
+                .map(|ch| ((ch as u8) - b'0') as usize)
+                .collect(),
+        };
+        let expected = vec!["2858".to_string()];
+        assert_eq!(expected, aoc.part2());
     }
 }
