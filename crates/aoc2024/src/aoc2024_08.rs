e75af4a8fe89f5c1413c20eeb91a532980aaecc0
Uncle Scientist <unclescientist@gmail.com>
2024-12-10 08:49:37 -0500

    2024 day 8 part 2

--- crates/aoc2024/src/aoc2024_08.rs
+++ crates/aoc2024/src/aoc2024_08.rs
@@ -17,6 +17,35 @@ impl Aoc2024_08 {
     fn in_bounds(&self, loc: &(i64, i64)) -> bool {
         loc.0 >= 0 && loc.1 >= 0 && loc.0 < self.rows && loc.1 < self.cols
     }
+
+    fn get_freq(&self, mut loc: (i64, i64), rise: i64, run: i64, all: bool) -> Vec<(i64, i64)> {
+        let mut result = Vec::new();
+        loc.0 += rise;
+        loc.1 += run;
+
+        while self.in_bounds(&loc) {
+            result.push(loc);
+            loc.0 += rise;
+            loc.1 += run;
+            if !all {
+                break;
+            }
+        }
+        result
+    }
+
+    fn antinodes(&self, list: &Vec<(i64, i64)>, all: bool) -> Vec<(i64, i64)> {
+        let mut result = Vec::new();
+        for i in 0..list.len() - 1 {
+            for j in i + 1..list.len() {
+                let rise = list[i].0 - list[j].0;
+                let run = list[i].1 - list[j].1;
+                result.extend(self.get_freq(list[i], rise, run, all));
+                result.extend(self.get_freq(list[j], -rise, -run, all));
+            }
+        }
+        result
+    }
 }
 
 impl Runner for Aoc2024_08 {
@@ -26,6 +55,7 @@ impl Runner for Aoc2024_08 {
 
     fn parse(&mut self) {
         let lines = aoclib::read_lines("input/2024-08.txt");
+        let _lines = aoclib::read_lines("test08.txt");
 
         self.rows = lines.len() as i64;
         self.cols = lines[0].len() as i64;
@@ -42,27 +72,17 @@ impl Runner for Aoc2024_08 {
 
     fn part1(&mut self) -> Vec<String> {
         let mut antinodes = HashSet::new();
-        for list in self.grid.values() {
-            for i in 0..list.len() - 1 {
-                for j in i + 1..list.len() {
-                    let rise = list[i].0 - list[j].0;
-                    let run = list[i].1 - list[j].1;
-                    let l1 = (list[i].0 + rise, list[i].1 + run);
-                    let l2 = (list[j].0 - rise, list[j].1 - run);
-
-                    if self.in_bounds(&l1) {
-                        antinodes.insert(l1);
-                    }
-                    if self.in_bounds(&l2) {
-                        antinodes.insert(l2);
-                    }
-                }
-            }
+        for nodes in self.grid.values().map(|list| self.antinodes(list, false)) {
+            antinodes.extend(nodes);
         }
         aoclib::output(antinodes.len())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let mut antinodes = HashSet::new();
+        for nodes in self.grid.values().map(|list| self.antinodes(list, true)) {
+            antinodes.extend(nodes);
+        }
+        aoclib::output(antinodes.len())
     }
 }
