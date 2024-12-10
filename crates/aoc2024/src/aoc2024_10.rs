7bf0186d644363e1fabb76705a87cb00caf6918b
Uncle Scientist <unclescientist@gmail.com>
2024-12-10 08:00:50 -0500

    2024 day 10 part 1

---
+++ crates/aoc2024/src/aoc2024_10.rs
@@ -0,0 +1,79 @@
+use std::collections::HashSet;
+
+use aoclib::Runner;
+
+#[derive(Default)]
+pub struct Aoc2024_10 {
+    grid: Vec<Vec<u8>>,
+    rows: i64,
+    cols: i64,
+}
+
+impl Aoc2024_10 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn count_paths(&self, row: i64, col: i64) -> usize {
+        let mut stack = Vec::new();
+        let mut visited = HashSet::new();
+
+        let mut found = 0;
+        stack.push((row, col, 0));
+        while let Some((r, c, level)) = stack.pop() {
+            if visited.insert((r, c)) {
+                if level == 9 {
+                    found += 1;
+                    continue;
+                }
+                for dir in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                    let new_row = r + dir.0;
+                    let new_col = c + dir.1;
+                    if new_row < 0 || new_col < 0 || new_row >= self.rows || new_col >= self.cols {
+                        continue;
+                    }
+                    if self.grid[new_row as usize][new_col as usize] == level + 1 {
+                        stack.push((new_row, new_col, level + 1));
+                    }
+                }
+            }
+        }
+
+        found
+    }
+}
+
+impl Runner for Aoc2024_10 {
+    fn name(&self) -> (usize, usize) {
+        (2024, 10)
+    }
+
+    fn parse(&mut self) {
+        let lines = aoclib::read_lines("input/2024-10.txt");
+
+        self.rows = lines.len() as i64;
+        self.cols = lines[0].len() as i64;
+
+        for line in lines {
+            self.grid
+                .push(line.chars().map(|ch| ch as u8 - b'0').collect())
+        }
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        let mut total = 0;
+
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                if self.grid[row as usize][col as usize] == 0 {
+                    total += self.count_paths(row, col);
+                }
+            }
+        }
+        aoclib::output(total)
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        aoclib::output("unsolved")
+    }
+}
--- crates/aoc2024/src/main.rs
+++ crates/aoc2024/src/main.rs
@@ -9,7 +9,7 @@ mod aoc2024_06;
 mod aoc2024_07;
 // mod aoc2024_08;
 mod aoc2024_09;
-// mod aoc2024_10;
+mod aoc2024_10;
 // mod aoc2024_11;
 // mod aoc2024_12;
 // mod aoc2024_13;
@@ -33,7 +33,7 @@ use aoc2024_06::*;
 use aoc2024_07::*;
 // use aoc2024_08::*;
 use aoc2024_09::*;
-// use aoc2024_10::*;
+use aoc2024_10::*;
 // use aoc2024_11::*;
 // use aoc2024_12::*;
 // use aoc2024_13::*;
@@ -62,7 +62,7 @@ fn run_2024(which: Selector) {
     let mut day07 = Aoc2024_07::new();
     //     let mut day08 = Aoc2024_08::new();
     let mut day09 = Aoc2024_09::new();
-    //     let mut day10 = Aoc2024_10::new();
+    let mut day10 = Aoc2024_10::new();
     //     let mut day11 = Aoc2024_11::new();
     //     let mut day12 = Aoc2024_12::new();
     //     let mut day13 = Aoc2024_13::new();
@@ -78,13 +78,13 @@ fn run_2024(which: Selector) {
     //     let mut day23 = Aoc2024_23::new();
 
     let mut days: Vec<&mut dyn Runner> = vec![
-        &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06,
-        // &mut day08,
+        &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
         &mut day09,
-        // &mut day10, &mut day11, &mut day14, &mut day13, &mut day12,
+        &mut day10,
+        // &mut day08,
+        // &mut day11, &mut day14, &mut day13, &mut day12,
         // &mut day15, &mut day16, &mut day17, &mut day18, &mut day19, &mut day20, &mut day21,
         // &mut day22, &mut day23,
-        &mut day07,
     ];
 
     match which {
