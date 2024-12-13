45537508f8d7d6b66f7a21078e680cc5b93187e3
Uncle Scientist <unclescientist@gmail.com>
2024-12-13 08:02:43 -0500

    2024 day 13 part 2

--- crates/aoc2024/src/aoc2024_13.rs
+++ crates/aoc2024/src/aoc2024_13.rs
@@ -28,7 +28,7 @@ impl Runner for Aoc2024_13 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        aoclib::output(self.claws.iter().filter_map(Claw::calc_scaled).sum::<i64>())
     }
 }
 
@@ -65,10 +65,10 @@ impl Claw {
     //       ax * by - ay * bx
     //
     //  m = (see above)
-    fn calc_tokens(&self) -> Option<i64> {
+    fn calc_tokens_with_scale(&self, scale: i64) -> Option<i64> {
         let a = self.button_a;
         let b = self.button_b;
-        let prize = self.prize;
+        let prize = (self.prize.0 + scale, self.prize.1 + scale);
 
         let n = (a.0 * prize.1 - a.1 * prize.0) / (a.0 * b.1 - a.1 * b.0);
         let m = (prize.0 - b.0 * n) / a.0;
@@ -80,34 +80,12 @@ impl Claw {
         }
     }
 
-    fn _calc_recursive(
-        tok_a: i64,
-        tok_b: i64,
-        a: (i64, i64),
-        b: (i64, i64),
-        prize: (i64, i64),
-    ) -> Option<i64> {
-        println!("{a:?} {b:?} {prize:?}");
-        if tok_a > 100 || tok_b > 100 {
-            return None;
-        }
-        if prize == (0, 0) {
-            return Some(3 * tok_a + tok_b);
-        }
-        let solution_a = if prize.0 >= a.0 && prize.1 >= a.1 {
-            Self::_calc_recursive(tok_a + 1, tok_b, a, b, (prize.0 - a.0, prize.1 - a.1))
-        } else {
-            None
-        };
-        if solution_a.is_none() {
-            if prize.0 >= b.0 && prize.1 >= b.1 {
-                Self::_calc_recursive(tok_a, tok_b + 1, a, b, (prize.0 - b.0, prize.1 - b.1))
-            } else {
-                None
-            }
-        } else {
-            solution_a
-        }
+    fn calc_tokens(&self) -> Option<i64> {
+        self.calc_tokens_with_scale(0)
+    }
+
+    fn calc_scaled(&self) -> Option<i64> {
+        self.calc_tokens_with_scale(10_000_000_000_000)
     }
 }
 
