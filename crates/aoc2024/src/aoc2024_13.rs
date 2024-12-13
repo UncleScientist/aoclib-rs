909fd7464ccd61740feab5ee71fd4c3eb329604e
Uncle Scientist <unclescientist@gmail.com>
2024-12-13 07:59:03 -0500

    2024 day 13 part 1

--- crates/aoc2024/src/aoc2024_13.rs
+++ crates/aoc2024/src/aoc2024_13.rs
@@ -40,8 +40,74 @@ struct Claw {
 }
 
 impl Claw {
+    // m = number of times a is pushed
+    // n =   ^    ^    ^   b ^    ^
+    //
+    // px = ax * m + bx * n
+    // py = ay * m + by * n
+    //
+    // m = ( px - bx * n ) / ax
+    // m = ( py - by * n ) / ay
+    //
+    // (px - bx * n)   (py - by * n)
+    // ------------- = -------------
+    //      ax             ay
+    //
+    //  ay * (px - bx * n) = ax * (py - by * n)
+    //  ay * px - ay * bx * n = ax * py - ax * by * n
+    //
+    //  ax * by * n - ay * bx * n = ax * py - ay * px
+    //
+    //  n * (ax * by - ay * bx) = ax * py - ay * px
+    //
+    //       ax * py - ay * px
+    //  n =  -----------------
+    //       ax * by - ay * bx
+    //
+    //  m = (see above)
     fn calc_tokens(&self) -> Option<i64> {
-        Some(1)
+        let a = self.button_a;
+        let b = self.button_b;
+        let prize = self.prize;
+
+        let n = (a.0 * prize.1 - a.1 * prize.0) / (a.0 * b.1 - a.1 * b.0);
+        let m = (prize.0 - b.0 * n) / a.0;
+
+        if (a.0 * m + b.0 * n, a.1 * m + b.1 * n) == prize {
+            Some(3 * m + n)
+        } else {
+            None
+        }
+    }
+
+    fn _calc_recursive(
+        tok_a: i64,
+        tok_b: i64,
+        a: (i64, i64),
+        b: (i64, i64),
+        prize: (i64, i64),
+    ) -> Option<i64> {
+        println!("{a:?} {b:?} {prize:?}");
+        if tok_a > 100 || tok_b > 100 {
+            return None;
+        }
+        if prize == (0, 0) {
+            return Some(3 * tok_a + tok_b);
+        }
+        let solution_a = if prize.0 >= a.0 && prize.1 >= a.1 {
+            Self::_calc_recursive(tok_a + 1, tok_b, a, b, (prize.0 - a.0, prize.1 - a.1))
+        } else {
+            None
+        };
+        if solution_a.is_none() {
+            if prize.0 >= b.0 && prize.1 >= b.1 {
+                Self::_calc_recursive(tok_a, tok_b + 1, a, b, (prize.0 - b.0, prize.1 - b.1))
+            } else {
+                None
+            }
+        } else {
+            solution_a
+        }
     }
 }
 
@@ -69,5 +135,20 @@ impl FromStr for Claw {
             button_b,
             prize,
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1_example1() {
+        let claw = Claw {
+            button_a: (94, 34),
+            button_b: (22, 67),
+            prize: (8400, 5400),
+        };
+        assert_eq!(Some(280), claw.calc_tokens());
     }
 }
