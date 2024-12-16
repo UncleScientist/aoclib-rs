8505cbcdb9c0419afea7fc3225be2db129cafad5
Uncle Scientist <unclescientist@gmail.com>
2024-12-16 08:53:44 -0500

    2024 day 16 part 2 (need to reimplment my own dijkstra/astar tho)

--- crates/aoc2024/src/aoc2024_16.rs
+++ crates/aoc2024/src/aoc2024_16.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 use aoclib::Runner;
-use pathfinding::prelude::dijkstra;
+use pathfinding::prelude::{astar_bag_collect, dijkstra};
 
 #[derive(Default)]
 pub struct Aoc2024_16 {
@@ -16,6 +16,46 @@ impl Aoc2024_16 {
     pub fn new() -> Self {
         Self::default()
     }
+
+    fn succssors(&self, state: &State) -> Vec<(State, usize)> {
+        let mut result = Vec::<(State, usize)>::new();
+        let next = (state.pos.0 + state.dir.0, state.pos.1 + state.dir.1);
+        if !self.maze.contains(&next) {
+            result.push((
+                State {
+                    pos: next,
+                    dir: state.dir,
+                },
+                1,
+            ));
+        }
+
+        let left_turn = (-state.dir.1, state.dir.0);
+        let next = (state.pos.0 + left_turn.0, state.pos.1 + left_turn.1);
+        if !self.maze.contains(&next) {
+            result.push((
+                State {
+                    pos: next,
+                    dir: left_turn,
+                },
+                1001,
+            ));
+        }
+
+        let right_turn = (state.dir.1, -state.dir.0);
+        let next = (state.pos.0 + right_turn.0, state.pos.1 + right_turn.1);
+        if !self.maze.contains(&next) {
+            result.push((
+                State {
+                    pos: next,
+                    dir: right_turn,
+                },
+                1001,
+            ));
+        }
+
+        result
+    }
 }
 
 impl Runner for Aoc2024_16 {
@@ -25,6 +65,7 @@ impl Runner for Aoc2024_16 {
 
     fn parse(&mut self) {
         let lines = aoclib::read_lines("input/2024-16.txt");
+        let _lines = aoclib::read_lines("test16-1.txt");
 
         self.rows = lines.len() as i64;
         self.cols = lines[0].len() as i64;
@@ -50,45 +91,7 @@ impl Runner for Aoc2024_16 {
         };
         let Some((_, cost)) = dijkstra(
             &start_state,
-            |state| {
-                let mut result = Vec::<(State, usize)>::new();
-                let next = (state.pos.0 + state.dir.0, state.pos.1 + state.dir.1);
-                if !self.maze.contains(&next) {
-                    result.push((
-                        State {
-                            pos: next,
-                            dir: state.dir,
-                        },
-                        1,
-                    ));
-                }
-
-                let left_turn = (-state.dir.1, state.dir.0);
-                let next = (state.pos.0 + left_turn.0, state.pos.1 + left_turn.1);
-                if !self.maze.contains(&next) {
-                    result.push((
-                        State {
-                            pos: next,
-                            dir: left_turn,
-                        },
-                        1001,
-                    ));
-                }
-
-                let right_turn = (state.dir.1, -state.dir.0);
-                let next = (state.pos.0 + right_turn.0, state.pos.1 + right_turn.1);
-                if !self.maze.contains(&next) {
-                    result.push((
-                        State {
-                            pos: next,
-                            dir: right_turn,
-                        },
-                        1001,
-                    ));
-                }
-
-                result
-            },
+            |state| self.succssors(state),
             |state| state.pos == self.end,
         ) else {
             panic!("no path found");
@@ -97,7 +100,40 @@ impl Runner for Aoc2024_16 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        aoclib::output("unsolved")
+        let start_state = State {
+            pos: self.start,
+            dir: (0, 1),
+        };
+        let Some((result, _)) = astar_bag_collect(
+            &start_state,
+            |state| self.succssors(state),
+            |state| (state.pos.0.abs_diff(self.end.0) + state.pos.1.abs_diff(self.end.1)) as usize,
+            |state| state.pos == self.end,
+        ) else {
+            panic!("no path found");
+        };
+
+        let nodes = result
+            .iter()
+            .flat_map(|x| x.iter().map(|s| s.pos))
+            .collect::<HashSet<_>>();
+
+        /*
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                if nodes.contains(&(row, col)) {
+                    print!("O");
+                } else if self.maze.contains(&(row, col)) {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+        */
+
+        aoclib::output(nodes.len())
     }
 }
 
