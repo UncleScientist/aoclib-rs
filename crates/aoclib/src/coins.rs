f6af1c0e62dfa36d483d23b224c9a0777130958b
Uncle Scientist <unclescientist@gmail.com>
2025-08-25 08:05:12 -0400

    Remember previous solutions

--- crates/aoclib/src/coins.rs
+++ crates/aoclib/src/coins.rs
@@ -6,6 +6,7 @@ use std::{
 
 pub struct CoinChange<T> {
     coins: Vec<Option<T>>,
+    change: Vec<Option<T>>,
 }
 
 impl<T> CoinChange<T>
@@ -24,10 +25,11 @@ where
     pub fn new(coins: &[T]) -> Self {
         Self {
             coins: coins.iter().copied().map(|c| Some(c)).collect(),
+            change: Vec::new(),
         }
     }
 
-    pub fn min_coins(&self, amount: T) -> Option<T> {
+    pub fn min_coins(&mut self, amount: T) -> Option<T> {
         let zero: T = 0_usize.into();
         let one: T = 1_usize.into();
 
@@ -37,8 +39,10 @@ where
             return Some(zero);
         }
 
-        let mut change: Vec<Option<T>> = vec![None; 1 + index];
-        change[0] = Some(zero);
+        if self.change.len() <= 1 + index {
+            self.change.resize(1 + index, None);
+        }
+        self.change[0] = Some(zero);
 
         let mut i = one;
         while i <= amount {
@@ -48,12 +52,12 @@ where
                 {
                     let first: usize = (i - value).into();
                     let second: usize = i.into();
-                    if let Some(cur_count) = change[first] {
-                        if let Some(old_value) = change[second] {
+                    if let Some(cur_count) = self.change[first] {
+                        if let Some(old_value) = self.change[second] {
                             let ov: T = old_value;
-                            change[second] = Some(ov.min(cur_count + one))
+                            self.change[second] = Some(ov.min(cur_count + one))
                         } else {
-                            change[second] = Some(cur_count + one);
+                            self.change[second] = Some(cur_count + one);
                         }
                     }
                 }
@@ -61,7 +65,7 @@ where
             i += one;
         }
 
-        change[index]
+        self.change[index]
     }
 }
 
@@ -71,31 +75,31 @@ mod test {
 
     #[test]
     fn failed_test() {
-        let cc = CoinChange::new(&[186, 419, 83, 408]);
+        let mut cc = CoinChange::new(&[186, 419, 83, 408]);
         assert_eq!(Some(20), cc.min_coins(6249));
     }
 
     #[test]
     fn ex1() {
-        let cc = CoinChange::new(&[1, 2, 5]);
+        let mut cc = CoinChange::new(&[1, 2, 5]);
         assert_eq!(Some(3), cc.min_coins(11));
     }
 
     #[test]
     fn ex1a() {
-        let cc = CoinChange::new(&[1, 2, 5]);
+        let mut cc = CoinChange::new(&[1, 2, 5]);
         assert_eq!(Some(20), cc.min_coins(100));
     }
 
     #[test]
     fn ex2() {
-        let cc = CoinChange::new(&[2]);
+        let mut cc = CoinChange::new(&[2]);
         assert_eq!(None, cc.min_coins(3));
     }
 
     #[test]
     fn ex3() {
-        let cc = CoinChange::new(&[1]);
+        let mut cc = CoinChange::new(&[1]);
         assert_eq!(Some(0), cc.min_coins(0));
     }
 }
