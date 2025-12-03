f2fce37e5dabf0b63de4ec56a9daa611666c8c46
Uncle Scientist <unclescientist@gmail.com>
2025-12-03 07:14:54 -0500

    AoC 2025 day 3 part 1

---
+++ crates/aoc2025/src/aoc2025_03.rs
@@ -0,0 +1,87 @@
+use std::str::FromStr;
+
+use aoclib::{LineParser, Runner};
+
+#[derive(Default)]
+pub struct Aoc2025_03 {
+    power_banks: Vec<PowerBank>,
+}
+
+impl Aoc2025_03 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Runner for Aoc2025_03 {
+    fn name(&self) -> (usize, usize) {
+        (2025, 3)
+    }
+
+    fn parse(&mut self) {
+        let lines = aoclib::read_lines("input/2025-03.txt");
+        self.power_banks = lines.parse_lines();
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        aoclib::output(
+            self.power_banks
+                .iter()
+                .map(|bank| bank.best_pair())
+                .sum::<usize>(),
+        )
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        aoclib::output("unsolved")
+    }
+}
+
+#[derive(Debug)]
+struct PowerBank {
+    bank: Vec<u8>,
+}
+
+impl PowerBank {
+    fn best_pair(&self) -> usize {
+        let Some((pos, &first_digit)) = self.bank[0..self.bank.len() - 1]
+            .iter()
+            .enumerate()
+            .rev()
+            .max_by(|a, b| a.1.cmp(b.1))
+        else {
+            panic!("no digits found");
+        };
+        let &second_digit = self.bank[pos + 1..].iter().max().unwrap();
+        first_digit as usize * 10 + second_digit as usize
+    }
+}
+
+impl FromStr for PowerBank {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(PowerBank {
+            bank: s.chars().map(|ch| ch as u8 - b'0').collect(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let test_data = [
+            ("987654321111111", 98),
+            ("811111111111119", 89),
+            ("234234234234278", 78),
+            ("818181911112111", 92),
+        ];
+        for test in test_data {
+            let bank: PowerBank = test.0.parse().unwrap();
+            assert_eq!(test.1, bank.best_pair());
+        }
+    }
+}
--- crates/aoc2025/src/main.rs
+++ crates/aoc2025/src/main.rs
@@ -2,8 +2,8 @@ use aoclib::{Runner, Selector};
 
 mod aoc2025_01;
 mod aoc2025_02;
-/*
 mod aoc2025_03;
+/*
 mod aoc2025_04;
 mod aoc2025_05;
 mod aoc2025_06;
@@ -17,8 +17,8 @@ mod aoc2025_12;
 
 use aoc2025_01::*;
 use aoc2025_02::*;
-/*
 use aoc2025_03::*;
+/*
 use aoc2025_04::*;
 use aoc2025_05::*;
 use aoc2025_06::*;
@@ -44,8 +44,8 @@ fn main() {
 fn run_2025(which: Selector) {
     let mut day01 = Aoc2025_01::new();
     let mut day02 = Aoc2025_02::new();
-    /*
     let mut day03 = Aoc2025_03::new();
+    /*
     let mut day04 = Aoc2025_04::new();
     let mut day05 = Aoc2025_05::new();
     let mut day06 = Aoc2025_06::new();
@@ -58,8 +58,8 @@ fn run_2025(which: Selector) {
     */
 
     let mut days: Vec<&mut dyn Runner> = vec![
-        &mut day01,
-        &mut day02, /* &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
+        &mut day01, &mut day02,
+        &mut day03, /* &mut day04, &mut day05, &mut day06, &mut day07,
                    &mut day09, &mut day08, &mut day10, &mut day11, &mut day12, */
     ];
 
